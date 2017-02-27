faff6c3a4a2c91d140c936c6a47007ea2baec402
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2017-02-27 02:52:44 +0000

    Added MidiEvent class functionality. Trying to play to stdout the same way I would play to keyboard, but not working.

--- Music/MidiUtil.py
+++ Music/MidiUtil.py
@@ -18,6 +18,13 @@ class MidiEvent:
         self.event = event
         self.data3 = data3
         self.timestamp = timestamp
+        self.loudness = 64  # max is 127
+
+        self.status_name = "note" if status == 144 else "instrument" if status == 192 else "unknown_status"
+        if self.status_name == "note":
+            self.event_name = "note_on" if self.event == 75 else "note_off" if self.event == 0 else "unknown_event"
+        else:
+            self.event_name = "N/A"
 
     def to_raw_data(self):
         return [[self.status, self.pitch, self.event, self.data3], self.timestamp]
@@ -27,6 +34,39 @@ class MidiEvent:
         lst, timestamp = data
         args = lst + [timestamp]
         return MidiEvent(*args)
+
+    @staticmethod
+    def from_data_list(lst):
+        return [MidiEvent.from_raw_data(x) for x in lst]
+
+    def is_note(self):
+        return self.status_name == "note"
+
+    def is_instrument(self):
+        return self.status_name == "instrument"
+
+    def invert_pitch(self, pivot):
+        if self.is_note():
+            new_pitch = pivot + (pivot - self.pitch)
+        return self
+
+    def add_time(self, time):
+        self.timestamp += time
+        return self
+
+    # def send_to_standard_out(self, player):
+    #     if self.is_note():
+    #         if self.event_name == "note_on":
+    #             player.note_on(self.pitch, self.loudness)
+    #         elif self.event_name == "note_off":
+    #             player.note_off(self.pitch, self.loudness)
+    #         else:
+    #             raise Exception("cannot send event: {}".format(self.to_raw_data()))
+    #     elif self.is_instrument():
+    #         player.set_instrument(self.pitch)
+
+    def __repr__(self):
+        return str(self.to_raw_data())
 
 
 def get_input_and_output_devices():
@@ -84,6 +124,35 @@ def send_notes_to_midi_out(notes, midi_output):
         note.output_to_midi(midi_output)
 
 
+def send_events_to_standard_out(events):
+    player = midi.Output(0)
+    data = [x.to_raw_data() for x in events]
+    send_data_to_midi_out(data, player)
+    return    
+
+    # pygame_time_ms = midi.time()
+    # transform = lambda event: event.add_time(pygame_time_ms + 1000)
+    # events = [transform(x) for x in events]
+
+    # final_timestamp = events[-1].timestamp
+
+    # for event in events:
+    #     print(event)
+    #     event.send_to_standard_out(player)
+
+    # # kill time so program doesn't end before midi is done playing
+
+    # # i = 0
+    # # MAX_OUTPUT_LENGTH = 1024  # due to pypm
+    # # while i < len(data):
+    # #     sub_data = data[i: i + MAX_OUTPUT_LENGTH]
+    # #     midi_output.write(sub_data)
+    # #     i += MAX_OUTPUT_LENGTH
+
+    # while midi.time() < final_timestamp:
+    #     time.sleep(0.1)
+
+
 def read_data_from_midi_in(midi_input, max_silence_seconds):
     data = []
     t0 = time.time()
@@ -96,17 +165,10 @@ def read_data_from_midi_in(midi_input, max_silence_seconds):
             new_data = new_data[0]
             data.append(new_data)
             print(new_data)
-            lst, timestamp_ms = new_data
-            last_time = timestamp_ms
-            status, data1, data2, data3 = lst
-            status_name = "note" if status == 144 else "instrument" if status == 192 else "unknown_status"
-            pitch = data1
-            event = data2
-            if status_name == "note":
-                event_name = "note_on" if data2 == 75 else "note_off" if data2 == 0 else "unknown_event"
-            else:
-                event_name = "N/A"
-            note_imbalance += 1 if event_name == "note_on" else -1 if event_name == "note_off" else 0
+            event = MidiEvent.from_raw_data(new_data)
+            last_time = event.timestamp
+            
+            note_imbalance += 1 if event.event_name == "note_on" else -1 if event.event_name == "note_off" else 0
             # ? = data3
             print("notes on:", note_imbalance)
         elif note_imbalance == 0 and last_time is not None and midi.time() - last_time > max_silence_seconds * 1000:
@@ -147,36 +209,35 @@ def load_data_from_datetime_string(s):
 
 
 def invert_data(data, pivot):
-    # stuff like this makes me want to implement a class; do it if there is too much hacking around with lists like this
-    def invert_pitch(datum):
-        lst, t = datum
-        res = [lst[0]] + [pivot + (pivot - lst[1])] + lst[2:]
-        return [res, t]
-
-    return [invert_pitch(x) for x in data]
+    lst = MidiEvent.from_data_list(data)
+    lst = [x.invert_pitch(pivot) for x in lst]
+    return [x.to_raw_data() for x in lst]
 
 
 if __name__ == "__main__":
-    max_silence_seconds = 15
+    max_silence_seconds = 1
 
     try:
-        inp, outp = get_input_and_output_devices()
-        print(inp, outp)
+        # inp, outp = get_input_and_output_devices()
+        # print(inp, outp)
 
         # notes = read_notes_from_midi_in(inp, timeout_seconds)
 
         # data = read_data_from_midi_in(inp, max_silence_seconds)
         # dump_data(data)
 
-        # data = load_random_data()
+        data = load_random_data()
         # data = invert_data(data, 66)
 
-        datetime_str = "20170220-010435"
-        data = load_data_from_datetime_string(datetime_str)
-        send_data_to_midi_out(data, outp)
+        # datetime_str = "20170220-010435"
+        # data = load_data_from_datetime_string(datetime_str)
+        # send_data_to_midi_out(data, outp)
+        events = MidiEvent.from_data_list(data)
+        send_events_to_standard_out(events)
 
     except:
         raise
     finally:
-        inp.close()
-        outp.close()
+        pass
+        # inp.close()
+        # outp.close()
