def1474243669e47c094677237f4593f78dd77ee
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2017-04-18 21:29:23 +0000

    Qualification C runs but is too slow for large coinLength

--- GoogleCodeJamPractice/GCJ_2016_Qualification_C.java
+++ GoogleCodeJamPractice/GCJ_2016_Qualification_C.java
@@ -2,6 +2,7 @@ import java.lang.Math;
 import java.util.ArrayList; 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 
 class PrimeNumbers {
@@ -49,10 +50,92 @@ class GCJ_2016_Qualification_C {
         return result;
     }
 
+    static boolean[] convertStringToCoin(String s) {
+        boolean[] result = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != '1' && c != '0') {
+                throw new RuntimeException(String.format("invalid string %s (char %c at index %d)", s, c, i));
+            }
+            result[i] = (c == '1');
+        }
+        return result;
+    }
+
+    static String convertCoinToString(boolean[] coin) {
+        String result = "";
+        for (int i = 0; i < coin.length; i++) {
+            result += (coin[i] ? "1" : "0");
+        }
+        return result;
+    }
+
+    static int[] getBaseValues(boolean[] coin) {
+        int[] result = new int[9];
+        for (int i = 2; i < 11; i++) {
+            int n = convertToBase(coin, i);
+            result[i - 2] = n;
+        }
+        return result;
+    }
+
+    static int[] getNonTrivialDivisors(int[] coinValues, PrimeNumbers primes) {
+        int[] result = new int[9];
+        for (int i = 0; i < 9; i++) {
+            int val = coinValues[i];
+            int divisor = primes.getLowestPrimeFactor(val);
+            if (divisor == val) {
+                // not a jamcoin because value in one of the bases is prime
+                return null;
+            }
+            result[i] = divisor;
+        }
+        return result;
+    }
+
+    static boolean[] getRandomCoin(int length) {
+        boolean[] result = new boolean[length];
+        result[0] = result[length - 1] = true;
+        Random random = new Random();
+        for (int i = 1; i < length - 1; i++) {
+            result[i] = random.nextBoolean();
+        }
+        return result;
+    }
+
+    static String[] getOutput(int coinLength, int nCoins, PrimeNumbers primes) {
+        String[] result = new String[nCoins];
+        List<String> seenCoins = new ArrayList<>();
+        // for (int i = 0; i < nCoins; i++) {
+            // boolean[] coin;
+            // int[] divisors;
+            int i = 0;
+            while (true) {
+                boolean[] coin = getRandomCoin(coinLength);
+                String s = convertCoinToString(coin);
+                boolean coinSeen = seenCoins.contains(s);
+                if (coinSeen) continue;
+                seenCoins.add(s);
+                int[] values = getBaseValues(coin);
+                int[] divisors = getNonTrivialDivisors(values, primes);
+                if (divisors == null) continue;
+                for (int divisor : divisors) {
+                    s += String.format(" %d", divisor);
+                }
+                
+                System.out.println(i + ". found new coin: " + s);
+                result[i] = s;
+                i++;
+                if (i >= nCoins) break;
+            }
+        // }
+        return result;
+    }
+
     public static void main(String[] args) {
         PrimeNumbers primes = new PrimeNumbers();
-        int n = 49;
-        int p = primes.getLowestPrimeFactor(n);
-        System.out.println(p);
+        int coinLength = Integer.parseInt(args[0]);
+        int nCoins = Integer.parseInt(args[1]);
+        String[] output = getOutput(coinLength, nCoins, primes);
     }
 }
