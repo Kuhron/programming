8f915268f36cf5ff0021e91463adbb75a2b6c18b
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2017-05-14 20:33:42 +0000

    moved bashrc here although git bash is being annoying about it

--- Music/PitchColorDisplay.py
+++ Music/PitchColorDisplay.py
@@ -1,5 +1,8 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+
+import Music.MusicalStructureUtil as structure
 
 
 PITCH_NUMBER_TO_COLOR = [
@@ -16,3 +19,34 @@ PITCH_NUMBER_TO_COLOR = [
     ( 70,  40,   0),  # R
     (120, 120, 120),  # B
 ]
+BLACK = (0, 0, 0)
+
+
+def create_rgb_array_from_pitch_classes(pitch_classes):
+    pitch_numbers = [structure.pitch_class_to_number(x) for x in pitch_classes]
+    colors = [PITCH_NUMBER_TO_COLOR[x] for x in pitch_numbers]
+    # TODO: make black border lines between colors
+    # just to get it to work: use one color
+    assert len(pitch_classes) == 1
+    color = colors[0]
+    height = 2 * len(colors) + 1
+    is_border_line = lambda y: y % 2 == 0
+    array = np.zeros((height, height, 3), "uint8")
+    array[:, [0, -1], :] = BLACK  # side columns
+    array[-1, 1: -1, :] = BLACK  # bottom
+    for i, color in enumerate(colors):
+        array[2 * i, 1: -1, :] = color
+        array[2 * i + 1, 1: -1, :] = BLACK
+    return array
+
+
+def show_image_for_pitch_classes(pitch_classes):
+    array = create_rgb_array_from_pitch_classes(pitch_classes)
+    im = Image.fromarray(array)
+    plt.imshow(im, interpolation="none")
+    plt.draw()
+
+
+if __name__ == "__main__":
+    pitch_classes = ["X"]
+    show_image_for_pitch_classes(pitch_classes)
