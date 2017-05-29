b3d0d221b18a5f109caa0bf64728fb35ce04b829
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2017-05-29 14:50:52 +0000

    more readability; still need to get last arg to be a lambda

--- Functional.py
+++ Functional.py
@@ -1,11 +1,12 @@
-run = lambda fs: fs[0](fs[1]()) if len(fs) == 2 else run(fs[:-1])(fs[-1])
+run = lambda fs: fs[0](fs[1]) if len(fs) == 2 else run(fs[:-1])(fs[-1])
+
+combinator = lambda f: lambda *args: f(f, *args)
+fibonacci_func = lambda f, n: [1] * (n + 1) if n <= 1 else (lambda x: x + [sum(x[-2:])])(f(f, n - 1))
 
 fs = (
-    (lambda f: lambda *args: f(f, *args)),
-    (lambda f, n: [1]*(n+1) if n<=1 else 
-        (lambda x: x + [sum(x[-2:])])(f(f, n-1))
-    ),
-    (lambda: 10),
+    (combinator),
+    (fibonacci_func),
+    (10),
 )
 
 print(run(fs))
