b816d114c980bc07deef309639f7ab46bbda6642
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2017-05-29 14:46:09 +0000

    Functional.py

--- Functional.py
+++ Functional.py
@@ -1,2 +1,11 @@
-class Function:
-    ?
+run = lambda fs: fs[0](fs[1]) if len(fs) == 2 else run(fs[:-1])(fs[-1])
+
+fs = (
+    (lambda f: lambda *_, **__: f(f, *_, **__)),
+    (lambda f, n: [1]*(n+1) if n<=1 else 
+        (lambda x: x + [sum(x[-2:])])(f(f, n-1))
+    ),
+    (10),
+)
+
+print(run(fs))
