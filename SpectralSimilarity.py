c2fa10f8cec7782ef389c88a113183387508d338
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2017-06-13 03:06:01 +0000

    works, yet to sanity check values

--- SpectralSimilarity.py
+++ SpectralSimilarity.py
@@ -14,8 +14,14 @@ import Music.WavUtil as wav
 # the statistic is the integral of the square of the difference between the y=x line and the other distribution on the plot
 
 
-def get_similarity_between_spectra(spec1, spec2):
-    return anderson_ksamp([spec1, spec2]).statistic  # who needs significance level
+def get_distance_between_samples(s1, s2):
+    return anderson_ksamp([s1, s2]).statistic  # who needs significance level
+
+
+def get_sample_from_spectrum(spec, freqs, n_observations):
+    # treat spectrum as pdf over the frequencies
+    norm_spec = spec / np.sum(spec)
+    return np.random.choice(freqs, p=norm_spec, size=(n_observations,))
 
 
 def convert_wav_to_spectrogram(filepath):
@@ -26,22 +32,40 @@ def convert_wav_to_spectrogram(filepath):
     # return spectrogram(array, fs=wav.RATE)
 
 
-def compare_spectrograms(spectrogram1, spectrogram2):
+def compare_spectrograms(spectrogram1, spectrogram2, freqs1, freqs2):
     shorter, longer = sorted([spectrogram1, spectrogram2], key=len)
     r = len(longer) *1.0/ len(shorter)
-    similarities = []
+    distances = []
+    n_observations = 100
     for i in range(len(shorter)):
+        # line up the time axes by just dilating (no translating or dilating by a changing factor yet)
+        # bootstrap some frequencies from each signal with probability=intensity
+        # compare these two samples' EDFs
         j = int(r * i)
-        similarity = get_similarity_between_spectra(shorter[i], longer[j])
-        similarities.append(similarity)
-    return np.mean(similarities)
+        pdf1 = shorter[:, i]
+        pdf2 = longer[:, j]
+        samp1 = get_sample_from_spectrum(pdf1, freqs1, n_observations)
+        samp2 = get_sample_from_spectrum(pdf2, freqs2, n_observations)
+        distance = get_distance_between_samples(samp1, samp2)
+        distances.append(distance)
+    return np.mean(distances)
 
 
 def compare_wavs(fp1, fp2):
     Pxx1, freqs1, bins1, im1 = convert_wav_to_spectrogram(fp1)
     Pxx2, freqs2, bins2, im2 = convert_wav_to_spectrogram(fp2)
-    res = compare_spectrograms(Pxx1, Pxx2)
+    res = compare_spectrograms(Pxx1, Pxx2, freqs1, freqs2)
     # assert res == compare_spectrograms(Pxx2, Pxx1)
+    return res
+
+
+def get_distance_matrix(fps):
+    n = len(fps)
+    res = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i + 1):
+            distance = compare_wavs(fps[i], fps[j])
+            res[i][j] = res[j][i] = distance
     return res
 
 
@@ -54,7 +78,9 @@ if __name__ == "__main__":
         "test5.wav",
     ]
     # files = random.sample(fps, 2)  # OverflowError with certain files?
-    files = fps[:2]
-    print(files)
-    similarity = compare_wavs(*files)
-    print(similarity)
+    # files = fps[:2]  # should work
+    # print(files)
+    # similarity = compare_wavs(*files)
+    # print(similarity)
+    np.set_printoptions(suppress=True)
+    print(get_distance_matrix(fps))
