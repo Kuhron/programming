75c3c883ee9f23760b7321e7665716b4a9192844
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2018-01-22 02:15:00 +0000

    better plots

--- RegimeChangingProcess.py
+++ RegimeChangingProcess.py
@@ -45,24 +45,69 @@ def get_step_with_restorative_force(x, center, max_step_size):
     else:
         return r(0, max_step_size)
 
+def get_count_generator():
+    x = 0
+    while True:
+        yield x
+        x += 1
+
 def get_time_series():
     x, a, b, c = 0, 0, 0, 0
+    float_format = "{:15.4f}"
     while True:
-        # print(x, a, b, c)
+        # print("".join([float_format.format(item) for item in [x, a, b]]))
         x_s = get_step_normal(a, b)
-        a_s = get_step_with_restorative_force(a, 0, 0.01) #get_step(b, c, x)
+        # a_s = get_step_with_restorative_force(a, 0, 0.01)
+        a_s = r(-0.01, 0.01)
         b_s = r(-0.01, 0.01) #get_step(c, x, a)
         # c_s = get_step_with_restorative_force(c, 0, 0.01) #get_step(x, a, b)
         x += x_s
         a += a_s
         b += b_s
         # c += c_s
-        yield x
+        yield (x, a, b, c)
+
+def run_realtime_plot(n_points, plot_window, plot_interval):
+    xg = get_count_generator()
+    yg = get_time_series()
+    plt.ion()
+    xs = [0] * plot_window
+    ys = [0] * plot_window
+    all_xs = []
+    all_ys = []
+    for i in range(n_points):
+        x = next(xg)
+        y = next(yg)[0]
+        xs.append(x)
+        ys.append(y)
+        xs = xs[1:]
+        ys = ys[1:]
+        all_xs.append(x)
+        all_ys.append(y)
+        if i % plot_interval == 0:
+            plt.gcf().clear()
+            plt.plot(xs, ys, color="b")
+            plt.pause(1e-12)
+
+def run_then_plot(n_points):
+    xs = [i for i in range(n_points)]
+    yg = get_time_series()
+    y_tuples = [next(yg) for x in xs]
+    ys = [y[0] for y in y_tuples]
+    mus = [y[1] for y in y_tuples]
+    log_sigmas = [y[2] for y in y_tuples]
+    plt.subplot(211)
+    plt.plot(xs, ys, label="y", color="b")
+    plt.legend()
+    plt.subplot(212)
+    plt.plot(xs, log_sigmas, label="log_sigma", color="r")
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
-    xs = [i for i in range(1000000)]
-    g = get_time_series()
-    ys = [next(g) for x in xs]
-    plt.plot(xs, ys)
-    plt.show()
+    n_points = 1000000
+    # plot_interval = 100
+    # plot_window = 10000
+    run_then_plot(n_points)
+    # run_realtime_plot(n_points, plot_window, plot_interval)
