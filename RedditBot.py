430c745cfbcf6d30b833b94df6966e57a40dc61b
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2018-11-24 22:05:29 +0000

    playing with reddit bot

--- RedditBot.py
+++ RedditBot.py
@@ -1,4 +1,4 @@
-# test reddit bot to message me things
+# test reddit bot
 
 import ast
 import praw
@@ -7,12 +7,26 @@ import threading
 import time
 
 
-r = praw.Reddit(user_agent="test")
 d = ast.literal_eval(open("_secret_TestBotInfo.txt").read().strip())
-r.login(d["bot"], d["password"])
+client_id = d["client_id"]
+client_secret = d["client_secret"]
+username = d["username"]
+password = d["password"]
+r = praw.Reddit(client_id=client_id, client_secret=client_secret, password=password, user_agent="test by /u/Wxyo", username=username)
+# r.login(username, d["password"])
+# verify logged in
+print("logged in as "+ r.user.me())
 
-r.send_message(
-    d["user"], 
-    "Test Message at {0}".format(time.strftime("%y-%m-%d-%H:%M:%S")),
-    "Here's a random number from 1 to 100: {0}.\n- your test bot".format(random.randrange(1,101))
-)
+def test_message():
+    # r.send_message(
+    #     d["user"], 
+    #     "Test Message at {0}".format(time.strftime("%y-%m-%d-%H:%M:%S")),
+    #     "Here's a random number from 1 to 100: {0}.\n- your test bot".format(random.randrange(1,101))
+    # )
+    r.redditor("Wxyo").message("TEST", "This happened!")
+
+def analyze_memeconomy():
+
+
+if __name__ == "__main__":
+    # test_message()
