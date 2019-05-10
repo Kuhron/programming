054e373078e11024c8e52637e62cad15412c5f71
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2019-05-10 21:26:28 +0000

    more ancestry

--- Ancestry.py
+++ Ancestry.py
@@ -9,21 +9,23 @@ def distance(xy1, xy2):
     dy = xy2[1] - xy1[1]
     return (dx**2 + dy**2) ** 0.5
 
-def average(xy1, xy2):
-    x = (xy1[0] + xy2[0]) * 0.5
-    y = (xy1[1] + xy2[1]) * 0.5
+def average(xy1, xy2, alpha=0.5):
+    x = alpha * xy1[0] + (1 - alpha) * xy2[0]
+    y = alpha * xy1[1] + (1 - alpha) * xy2[1]
     return (x, y)
 
 
 class Person:
     N_BASES = 50
     MINIMUM_REPRODUCTION_AGE = 3
+    MIN_SCORE = float("-inf")
 
     def __init__(self, dna, land, location):
         self.dna = dna
         self.land = land
         self.location = location
         self.age = 0
+        self.person_id = "{}-{}-{}".format(int(location[0]), int(location[1]), Person.value_gene_binary(self.dna))
 
         # genetic attributes
         self.similarity_dna = self.dna[:]
@@ -33,9 +35,10 @@ class Person:
         self.max_neighbors = Person.sum_gene(self.dna[33:43])
         self.ideal_similarity = Person.value_gene_01(self.dna[9:24])
         self.reproduction_probability = Person.value_gene_01(self.dna[41:47])
+        self.wanderlust_probability = Person.value_gene_01(self.dna[0:8])
 
     def __repr__(self):
-        return "Person\ncoords {}\nage {}\nDNA {}\n".format(self.location, self.age, self.dna)
+        return "Person ID {}\ncoords {}\nage {}\nDNA {}\n".format(self.person_id, self.location, self.age, self.dna)
 
     def get_similarity_weights(self):
         s = self.similarity_dna
@@ -52,20 +55,23 @@ class Person:
         return [w / total_weight for w in raw_weights]
 
     def select_mate(self, options):
-        best_score = math.nan
+        best_score = Person.MIN_SCORE
         best_option = None
         options = options[:]
         random.shuffle(options)
         for option in options:
             score = self.score(option)
-            if score > best_score:  # note NaN > NaN == False
+            # print("score {}".format(score))
+            if score > best_score:
+                # print("BETTER")
                 best_score = score
                 best_option = option
         return best_option
 
     def score(self, other):
-        if other is self:
-            return math.nan
+        if other.dna == self.dna:
+            # no mating with yourself or your clone
+            return Person.MIN_SCORE
         # select part of the dna for use as "similarity", not all of it
         d = Person.genetic_distance(self.similarity_dna, other.similarity_dna, weights=self.similarity_weights)
         s = self.ideal_similarity
@@ -77,21 +83,26 @@ class Person:
 
     def reproduce(self, mate):
         new_dna = Person.combine_dna(self.dna, mate.dna)
-        new_location = average(self.location, mate.location)
+        new_location = average(self.location, mate.location, alpha=random.random())
         return Person(new_dna, self.land, new_location)
 
-    def maybe_reproduce(self, people):
+    def maybe_reproduce(self):
         # decide whether to reproduce (based on something in DNA)
         offspring = None
+        options = self.land.get_mating_options(self)
         will_reproduce = (not self.is_baby()) and self.check_dna_will_reproduce()
         if will_reproduce:
-            mate = self.select_mate(people)
+            # print("will reproduce")
+            mate = self.select_mate(options)
             if mate is not None:
+                # print("got one!")
                 offspring = self.reproduce(mate)
             else:
+                # print("but got none")
                 offspring = None
                 self.move()  # find more fertile area
         self.age += 1
+        # print("returning offspring {}".format(offspring))
         return offspring
 
     def check_dna_will_reproduce(self):
@@ -109,13 +120,18 @@ class Person:
         return self.is_old() or self.is_overcrowded()
 
     def is_old(self):
+        # if self.is_baby():
+        #     return False
         return random.uniform(0, self.age) > self.genetic_age
 
     def is_overcrowded(self):
-        if self.is_baby():
-            return False
         neighbors = self.land.get_mating_options(self)
         return random.uniform(0, len(neighbors)) > self.max_neighbors
+
+    def maybe_move(self):
+        # prevent clones from just clustering together in the same place
+        if random.random() < self.wanderlust_probability:
+            self.move()
 
     def move(self):
         if self.is_baby():
@@ -164,6 +180,7 @@ class Land:
         self.people = []
         self.year = 0
         self.population_history = [0]
+        self.next_person_id = 0
 
     def populate(self, n_people):
         for _ in range(n_people):
@@ -184,26 +201,34 @@ class Land:
 
     def add_person(self, person):
         self.people.append(person)
+        # print("born {} --> population {}".format(person, len(self.people)))
 
     def remove_person(self, person):
         self.people.remove(person)
+        # print("died {} --> population {}".format(person, len(self.people)))
 
     def get_mating_options(self, person):
         return [p for p in self.people if distance(person.location, p.location) <= person.neighbor_radius]
 
     def go_to_next_generation(self):
         print("year {}".format(self.year))
+        self.move_people()
         self.add_people()
         self.kill_people()
         self.year += 1
         print("made it to year {}. there are now {} people".format(self.year, len(self.people)))
         self.population_history.append(len(self.people))
 
+    def move_people(self):
+        for p in self.people:
+            p.maybe_move()
+
     def add_people(self):
         people_to_add = []
         for person in self.people:
-            p = person.maybe_reproduce(self.people)
+            p = person.maybe_reproduce()
             if p is not None:
+                # print("got baby {}".format(p))
                 people_to_add.append(p)
         for p in people_to_add:
             self.add_person(p)
@@ -225,6 +250,12 @@ class Land:
         age_nums = [256 * age / max_age for age in ages]
         colors = [age_cmap(n) for n in age_nums]
         plt.scatter(xs, ys, alpha=0.5, c=colors)
+
+        # circles for their radii
+        for p in self.people:
+            circle = plt.Circle(p.location, p.neighbor_radius, color="r", alpha=0.05)
+            plt.gca().add_artist(circle)
+
         plt.title("people locations")
         plt.show()
 
@@ -246,8 +277,16 @@ class Land:
             print("everyone is dead")
         else:
             genomes = sorted(p.dna for p in self.people)
+            d = {}
             for genome in genomes:
-                print(genome)
+                if genome not in d:
+                    d[genome] = 0
+                d[genome] += 1
+            for genome in sorted(d):
+                if d[genome] > 1:
+                    print("{} (total clones: {})".format(genome, d[genome]))
+                else:
+                    print("{} (unique)".format(genome))
             self.plot_genome_average()
         print("--- end of genome report for year {} ---".format(self.year))
 
@@ -258,30 +297,54 @@ class Land:
                 g[i].append(int(b))
         g_avgs = []
         g_stds = []
+        g_0_count = []
+        g_1_count = []
         for lst in g:
             g_avgs.append(np.mean(lst))
             g_stds.append(np.std(lst))
+            g_0_count.append(lst.count(0))
+            g_1_count.append(lst.count(1))
+
+        digs = math.ceil(math.log10(len(self.people)))
+        for i in range(Person.N_BASES):
+            s = "bit "
+            s += str(i).rjust(2)
+            s += ": (0 * "
+            s += str(g_0_count[i]).rjust(digs)
+            s += ") "
+            n_dashes = 25
+            s_dashes = "-" * n_dashes
+            avg_position = math.floor(g_avgs[i] * n_dashes)
+            s_dashes = s_dashes[:avg_position] + "X" + s_dashes[avg_position+1:]
+            s += s_dashes
+            s += " (1 * "
+            s += str(g_1_count[i]).rjust(digs)
+            s += ")"
+            print(s)
+
         g_minus_1_sd = [max(0, g_avgs[i] - g_stds[i]) for i in range(Person.N_BASES)]
         g_plus_1_sd = [min(1, g_avgs[i] + g_stds[i]) for i in range(Person.N_BASES)]
         xs = list(range(Person.N_BASES))
-        plt.bar(xs, g_plus_1_sd, color="g")
-        plt.bar(xs, g_avgs, color="y")
-        plt.bar(xs, g_minus_1_sd, color="r")
+        plt.bar(xs, g_plus_1_sd, color="#59ba57")
+        plt.bar(xs, g_avgs, color="#e5cd19")
+        plt.bar(xs, g_minus_1_sd, color="white")
         plt.title("genome bit values")
         plt.show()
 
+    def report(self):
+        self.plot_people_locations()
+        self.plot_ages()
+        self.report_genomes()
+
 
 if __name__ == "__main__":
-    land = Land(20, 20)
+    land = Land(30, 30)
     land.populate(100)
-    for _ in range(50):
+    for _ in range(100):
         land.go_to_next_generation()
-        if land.year % 5 == 0:
-            land.plot_people_locations()
-            land.report_genomes()
+        if land.year % 10 == 0:
+            # land.report()
             pass
     for person in land.people:
         print(person)
-    print("there are {} people".format(len(land.people)))
-    land.plot_population_history()
-    land.report_genomes()
+    land.report()
