e75646ada84fa5f45cbf2c6dabe126b4439d7293
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2019-12-05 05:17:05 +0000

    add js to go to random language wikipedia page

---
+++ RandLang.js
@@ -0,0 +1,3 @@
+// save this as a bookmark in Firefox
+// the location is:
+javascript:letters = "abcdefghijklmnopqrstuvwxyz"; function f() {return letters[Math.floor(Math.random()*26)];} window.location.href = "https://en.wikipedia.org/w/index.php?title=ISO_639:"+f()+f()+f();
