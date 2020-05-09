9990c4048f15597c938ef9bcf7748abd371aa16d
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2020-05-09 19:58:58 +0000

    added variability in score functions

--- Genetic2.py
+++ Genetic2.py
@@ -67,6 +67,7 @@ def get_trajectory(g, initial_condition):
     if constituents is None:
         # genome did not terminate, kill the organism
         return None
+    # print("{} constituents".format(len(constituents)))
     x = initial_condition
     t_max = 100
     xs = [x]
@@ -100,15 +101,44 @@ def select_environmental_survivors(organisms):
 
 
 def trajectory_survives(xs):
-    return all(0 <= x <= 1 for x in xs) and np.std(xs) > 0
+    # minimum requirements for survival, will be further culled based on score later
+    min_val = -1000
+    max_val = 1000
+    return all(min_val <= x <= max_val for x in xs) and np.std(xs) > 0
 
 
 def score_trajectory(xs):
-    # if not(trajectory_survives(xs)):
-    #     return 0
+    # use component simpler scoring functions, can play with their weights
+    components = []
+    components.append(mean_abs_diff_score(xs, 1) * 1)
+    components.append(mean_abs_diff_score(xs, 2) * -1)
+    components.append(mean_abs_diff_score(xs, 3) * 1)
+    # components.append(sign_diff_score(xs, 1, 1))
+
+    return sum(components)
+
+
+def mean_abs_diff_score(xs, n):
     # reward lots of movement, less boring processes
-    dxs = np.diff(xs)
+    dxs = np.diff(xs, n)
     return np.mean(abs(dxs))
+
+
+def sign_diff_score(xs, n_x_diff, n_sign_diff):
+    # higher score for the sign switching, i.e., oscillation in the trajectory
+    dxs = np.diff(xs, n_x_diff)
+    signs = np.sign(dxs)
+    d_signs = np.diff(signs, n_sign_diff)
+    return np.mean(abs(d_signs))
+
+
+def mutate(g):
+    # flip bits at random, but only do it rarely
+    while random.random() < 0.03:
+        i = random.choice(range(len(g)))
+        other_bit = {"0":"1", "1":"0"}[g[i]]
+        g = g[:i] + other_bit + g[i+1:]
+    return g
 
 
 def get_next_generation(organisms):
@@ -123,6 +153,8 @@ def get_next_generation(organisms):
         # put both of these children in there
         child_a = g[:cut_point_0] + mate[cut_point_1:]
         child_b = mate[:cut_point_1] + g[cut_point_0:]
+        child_a = mutate(child_a)
+        child_b = mutate(child_b)
         all_this_generation.append(g)
         all_this_generation.append(child_a)
         all_this_generation.append(child_b)
@@ -173,6 +205,23 @@ def plot_trajectories(organisms):
     plt.show()
 
 
+def plot_sample_gallery(organisms):
+    if type(organisms) is dict:
+        organisms = list(organisms.keys())
+    rows = 3
+    columns = 6
+    n_panels = rows * columns
+    for i in range(n_panels):
+        organism_number = random.choice(range(len(organisms)))
+        g = organisms[organism_number]
+        initial_condition = random.random()
+        xs = get_trajectory(g, initial_condition)
+        plt.subplot(rows, columns, i+1)
+        plt.title("org {}: {:.3f}".format(organism_number, initial_condition))
+        plt.plot(xs)
+    plt.show()
+
+
 if __name__ == "__main__":
     organisms = [get_random_genome(100) for i in range(10)]
     for generation in range(100):
@@ -187,4 +236,5 @@ if __name__ == "__main__":
 
     plot_genes(sorted(organisms))
 
-    plot_trajectories(organisms)
+    # plot_trajectories(organisms)
+    plot_sample_gallery(organisms)
