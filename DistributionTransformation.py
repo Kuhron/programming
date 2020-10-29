f71cd25e320a5c714dbd80bd3ea5f31ecca07393
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2020-10-29 22:50:49 +0000

    playing with applying functions to distributions

---
+++ DistributionTransformation.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def apply(f, dist_f, n_samples=10000):
+    dist_samples = dist_f(size=n_samples)
+    transformed = f(dist_samples)
+    return transformed
+
+
+if __name__ == "__main__":
+    a = np.random.uniform(-10, 10)
+    b = np.random.uniform(0, 10)
+    dist_f = lambda size: np.random.normal(a, b, size=size)
+
+    # f = lambda x: abs(x)**0.5 - abs(x)**0.4
+    f = lambda x: x**-x
+
+    xs = apply(f, dist_f, n_samples=1000000)
+    plt.hist(xs, bins=100)
+    plt.show()
