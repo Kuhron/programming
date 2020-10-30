23feff722b7d882f9e589c2597aaa5325a083076
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2020-10-30 00:36:45 +0000

    version vimrc

---
+++ .vimrc
@@ -0,0 +1,12 @@
+set expandtab
+set tabstop=4
+set shiftwidth=4  " indentation amount
+set number
+
+" indent xml: https://stackoverflow.com/questions/21408222/vim-indent-xml-file/32405832
+:set equalprg=xmllint\ --format\ -
+
+" Allow saving of files as sudo when I forgot to start vim using sudo.
+" https://stackoverflow.com/questions/2600783/how-does-the-vim-write-with-sudo-trick-work
+cmap w!! w !sudo tee > /dev/null %
+
--- DistributionTransformation.py
+++ DistributionTransformation.py
@@ -11,10 +11,12 @@ def apply(f, dist_f, n_samples=10000):
 if __name__ == "__main__":
     a = np.random.uniform(-10, 10)
     b = np.random.uniform(0, 10)
-    dist_f = lambda size: np.random.normal(a, b, size=size)
+    # dist_f = lambda size: np.random.normal(a, b, size=size)
+    dist_f = lambda size: np.random.pareto(a=1, size=size)
 
     # f = lambda x: abs(x)**0.5 - abs(x)**0.4
-    f = lambda x: x**-x
+    # f = lambda x: x**-x
+    f = lambda x: np.log(x)
 
     xs = apply(f, dist_f, n_samples=1000000)
     plt.hist(xs, bins=100)
