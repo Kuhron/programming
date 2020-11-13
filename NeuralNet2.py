fixed gradient explosion problem by lowering learning rate, will need to have some hidden layers for better fits, but at least this simple one is able to improve a bit

NeuralNet2.py
@@ -36,12 +36,13 @@ if __name__ == "__main__":
     gradient_of_loss_function = gradient_of_squared_error
     # loss_multiple_data = lambda ys_p, ys: sum(loss_function(y_p, y) for y_p, y in zip(ys_p, ys)) / len(ys)
     # gradient_loss_multiple_data = lambda ys_p, ys: sum(gradient_of_loss_function(y_p, y) for y_p, y in zip(ys_p, ys)) / len(ys)
-    learning_rate = 0.01  # usually between 1e-6 and 0.1
+    learning_rate = 0.00001  # usually between 1e-6 and 0.1
 
 
     # try fitting a function of three input variables to one output variable
-    secret_data_function = lambda x0,x1,x2: np.sin(x0)**2 + np.cos(np.sin(x1)**2)**2 + -4*np.sin(np.cos(x0*x2)**2)
-    n_data_points = 100
+    # secret_data_function = lambda x0,x1,x2: np.sin(x0)**2 + np.cos(np.sin(x1)**2)**2 + -4*np.sin(np.cos(x0*x2)**2)
+    secret_data_function = lambda x0,x1,x2: x0*x1 + x1*x2**2 + np.sqrt(np.abs(x0+x1+x2))
+    n_data_points = 1000
     X_sample = [np.random.uniform(-10, 10, (3,)) for i in range(n_data_points)]
     y_sample = [secret_data_function(x0,x1,x2) for x0,x1,x2 in X_sample]
 
@@ -50,13 +51,17 @@ if __name__ == "__main__":
     inputs = X_sample
     weights = [[random.random() for i in range(n_input_neurons)] for j in range(n_receiving_neurons)]
     biases = [random.random() for i in range(n_receiving_neurons)]
+    loss_record = []  # loss per round
 
-    for round_i in range(10):
+    n_rounds = 20
+
+    for round_i in range(n_rounds):
         print("beginning round {}".format(round_i))
         outputs = []  # for each output neuron, a list of outputs (predicted ys) for each sample X
         raw_outputs = []  # without activation function, for later computation of f'(wx+b)
         losses = []
         gradient_losses = []
+
         for receiving_neuron_i in range(n_receiving_neurons):
             print("receiving neuron {}".format(receiving_neuron_i))
             weight_vector = weights[receiving_neuron_i]
@@ -70,25 +75,34 @@ if __name__ == "__main__":
             for sample_i in range(n_data_points):
                 input_vector_this_sample = inputs[sample_i]  # one value for each neuron in the input layer
                 inputs_to_this_neuron = input_vector_this_sample  # final values from last layer
+
                 raw_output_this_sample = get_neuron_output_raw(inputs_to_this_neuron, weight_vector, bias)
                 raw_outputs_this_neuron.append(raw_output_this_sample)
+
                 output_this_sample = activation_function(raw_output_this_sample)
                 outputs_this_neuron.append(output_this_sample)
+
                 correct_output = y_sample[sample_i]
+
                 loss_this_sample = loss_function(output_this_sample, correct_output)
                 losses_this_neuron.append(loss_this_sample)
+
                 gradient_loss_this_sample = gradient_of_loss_function(output_this_sample, correct_output)
                 gradient_losses_this_neuron.append(gradient_loss_this_sample)
-                print("raw {}, activated {}, correct {}, loss {}, gradloss {}".format(raw_output_this_sample, output_this_sample, correct_output, loss_this_sample, gradient_loss_this_sample))
+
+                print("input {}, raw out {}, activated {}, correct {}, loss {}, gradloss {}".format(input_vector_this_sample, raw_output_this_sample, output_this_sample, correct_output, loss_this_sample, gradient_loss_this_sample))
 
             outputs.append(outputs_this_neuron)
             raw_outputs.append(raw_outputs_this_neuron)
+
             mean_loss_over_samples_this_neuron = sum(losses_this_neuron) / len(losses_this_neuron)
             losses.append(mean_loss_over_samples_this_neuron)
+
             mean_gradient_loss_over_samples_this_neuron = sum(gradient_losses_this_neuron) / len(gradient_losses_this_neuron)
             gradient_losses.append(mean_gradient_loss_over_samples_this_neuron)
 
         loss_this_round = sum(losses)  # don't mean over neurons, but do mean over samples (within each neuron)
+        loss_record.append(loss_this_round)
         print("round {} has loss {}".format(round_i, loss_this_round))
 
         # adjust the weights
@@ -100,15 +114,18 @@ if __name__ == "__main__":
         for sample_i in range(n_data_points):
             X_vector = X_sample[sample_i]
             y_vector = y_sample[sample_i]
+
             for receiving_neuron_i in range(n_receiving_neurons):
                 raw_output = raw_outputs[receiving_neuron_i][sample_i]
                 y_pred = outputs[receiving_neuron_i][sample_i]
                 y = y_sample[sample_i]
                 weight_vector = weights[receiving_neuron_i]
                 bias = biases[receiving_neuron_i]
+
                 for weight_i in range(len(weight_vector)):
                     weight = weight_vector[weight_i]
                     input_from_previous_neuron = X_vector[weight_i]
+
                     # adjust the weight by (learning rate times negative of gradient of loss wrt this weight)
                     # by chain rule, dL/dw = dL/dy_predicted * dy_predicted/dw
                     d_loss_d_ypred = gradient_of_loss_function(y_pred, y)
@@ -119,8 +136,8 @@ if __name__ == "__main__":
                     d_ypred_d_w = derivative_of_activation_function(raw_output) * x   # chain rule on activation(w*x+b)
                     d_loss_d_w = d_loss_d_ypred * d_ypred_d_w
                     gradient_of_loss_wrt_weight = d_loss_d_w
-                    weight_adjustment = get_weight_adjustment(learning_rate, gradient_of_loss_wrt_weight)
 
+                    weight_adjustment = get_weight_adjustment(learning_rate, gradient_of_loss_wrt_weight)
                     weight_adjustments[receiving_neuron_i][weight_i] += weight_adjustment
 
                 # bias adjustment
@@ -128,6 +145,10 @@ if __name__ == "__main__":
                 d_loss_d_ypred = gradient_of_loss_function(y_pred, y)
                 d_ypred_d_b = derivative_of_activation_function(raw_output) * 1  # chain rule on activation(w*x+b)
                 d_loss_d_b = d_loss_d_ypred * d_ypred_d_b
+                gradient_of_loss_wrt_bias = d_loss_d_b
+
+                bias_adjustment = get_weight_adjustment(learning_rate, gradient_of_loss_wrt_bias)
+                bias_adjustments.append(bias_adjustment)
 
         # now actually change the weights and biases
         for weight_vector_i in range(len(weights)):
@@ -141,3 +162,7 @@ if __name__ == "__main__":
         print("done with round {}".format(round_i))
 
 
+    print("done training network")
+    plt.plot(loss_record)
+    plt.gca().set_yscale('log')
+    plt.show()