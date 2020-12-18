3dba8a21c2441933f4de54b771abdc1463dcc68c
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2020-12-18 07:05:24 +0000

    negative and positive masses, fun to watch

--- BasicGravitySimulation.py
+++ BasicGravitySimulation.py
@@ -10,14 +10,27 @@ class Particle:
         self.mass = mass
         self.position = position
         self.velocity = velocity
+        self.color = self.get_color()
     
     @staticmethod
-    def get_random_particle():
-        mass = np.random.uniform(0, 100)
-        position = np.random.uniform(-1, 1, (2,))
+    def get_random_particle(max_xy=1):
+        mass_sign = random.choice([-1, 1])
+        mass = 10 * np.random.pareto(a=1) * mass_sign
+        position = np.random.uniform(-max_xy, max_xy, (2,))
         # velocity = np.random.uniform(-1, 1, (2,))
         velocity = np.zeros((2,))
         return Particle(mass, position, velocity)
+
+    def get_color(self):
+        # if negative mass, make it more red
+        # if self.mass >= 0:
+        #     red = random.uniform(0, 0.3)
+        # else:
+        #     red = random.uniform(0.7, 1)
+        # green, blue = np.random.uniform(0, 1, (2,))
+        # return (red, green, blue)
+        return (1, 0, 0) if self.mass < 0 else (0, 0, 1)
+
 
     def move(self, displacement):
         self.position += displacement
@@ -29,6 +42,10 @@ class Particle:
 class UniverseState:
     def __init__(self, particles):
         self.particles = particles
+        self.min_x = 0
+        self.max_x = 0
+        self.min_y = 0
+        self.max_y = 0
 
     @staticmethod
     def get_random_initial_state(n_particles):
@@ -42,7 +59,15 @@ class UniverseState:
         return np.array([p.mass for p in self.particles])
 
     def get_positions(self):
-        return np.array([p.position for p in self.particles])
+        positions = np.array([p.position for p in self.particles])
+        xs = positions[:,0]  # all points, first item in 2d vector for that point
+        ys = positions[:,1]
+        self.min_x = min(self.min_x, min(xs))
+        self.max_x = max(self.max_x, max(xs))
+        self.min_y = min(self.min_y, min(ys))
+        self.max_y = max(self.max_y, max(ys))
+
+        return positions
 
     def get_distance_matrix(self):
         x = self.get_positions()
@@ -91,10 +116,19 @@ class UniverseState:
         distances = self.get_distance_matrix()
         radial_vectors = self.get_radial_vectors_between_particles()
         r_squareds = distances ** 2
+        force_factors = 1 / r_squareds
+
+        # make a possibly interesting modification: force goes up dramatically again once objects are too far away, keeping the system bound
+        # distant_distance = 10
+        # get_distant_attractive_force = lambda d: np.maximum(0, d - distant_distance) ** 2  # have to use maximum, not max, to compare array and scalar and get array output
+        # force_factors += get_distant_attractive_force(distances)
+
+
         mass_products = self.get_mass_product_matrix()
-        G = 1e-6
-        force_magnitudes = np.where(distances == 0, 0, G * mass_products / r_squareds)
-        forces = force_magnitudes[:, :, None] * -1 * radial_vectors  # for each cell in (n,n) grid, multiply 2d vector by 1d magnitude, so need to broadcast (n,n) magnitude array to (n,n,1)
+        G = 1e-5
+        gravity_direction = -1  # -1 for attractive, 1 for repulsive
+        force_magnitudes = np.where(distances == 0, 0, G * mass_products * force_factors)
+        forces = force_magnitudes[:, :, None] * gravity_direction * radial_vectors  # for each cell in (n,n) grid, multiply 2d vector by 1d magnitude, so need to broadcast (n,n) magnitude array to (n,n,1)
         forces_per_particle = forces.sum(axis=0)  # arr.sum(**kwargs) is same as np.sum(arr, **kwargs)
         masses = self.get_masses()
         accelerations = forces_per_particle / masses[:, None]
@@ -121,12 +155,42 @@ class UniverseState:
         self.accelerate_particles(accelerations)
         self.move_particles()
 
-    def plot(self, restrict_to_original_box=False):
+        n = len(self.particles)
+        # get rid of ones that are going too far away
+        self.prune(min_x=-10, max_x=10, min_y=-10, max_y=10)
+        self.add_new_particles(n)
+
+    def prune(self, min_x, max_x, min_y, max_y):
+        particles = []
+        for p in self.particles:
+            x, y = p.position
+            if min_x <= x <= max_x and min_y <= y <= max_y:
+                # keep it, it's within range
+                particles.append(p)
+        self.particles = particles
+
+    def add_new_particles(self, n_total_to_achieve):
+        n = len(self.particles)
+        n_to_make = n_total_to_achieve - n
+        if n <= 0:
+            return
+        new_particles = [Particle.get_random_particle(max_xy=10) for i in range(n_to_make)]
+        self.particles += new_particles
+
+    def plot(self, restrict_to_original_box=False, restrict_to_record_box=False):
         positions = self.get_positions()
-        plt.scatter(*positions.T)
+        masses = self.get_masses()
+        point_sizes = np.maximum(5, abs(masses) ** (2/3))  # point size is area of the dot on the plot, not radius
+        colors = [p.color for p in self.particles]
+        plt.scatter(*positions.T, s=point_sizes, c=colors)
         if restrict_to_original_box:
+            assert not restrict_to_record_box
             plt.xlim(-1, 1)
             plt.ylim(-1, 1)
+        elif restrict_to_record_box:
+            assert not restrict_to_original_box
+            plt.xlim(self.min_x, self.max_x)
+            plt.ylim(self.min_y, self.max_y)
 
 
 
@@ -136,13 +200,17 @@ if __name__ == "__main__":
 
     n_particles = 100
     state = UniverseState.get_random_initial_state(n_particles)
+
+    step_i = 0
     while True:
         if not plt.fignum_exists(fignum):
             print("user closed plot; exiting")
             break
+
         state.evolve()
-        plt.gcf().clear()
-        state.plot(restrict_to_original_box=False)
-        plt.draw()
-        plt.pause(0.001)
+        if step_i % 100 == 0:
+            plt.gcf().clear()
+            state.plot(restrict_to_original_box=False, restrict_to_record_box=True)
+            plt.draw()
+            plt.pause(0.001)
 
