6b4a8ed7989dd46dfc202d34e8d5cd6620e5a809
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2021-01-19 20:12:43 +0000

    forgot to save this

--- Minecraft/RaspberryJamScripts/maze_backrooms.py
+++ Minecraft/RaspberryJamScripts/maze_backrooms.py
@@ -6,32 +6,6 @@ import mcpi.block as block
 from mcutil import set_blocks_cuboid, get_spiral_xzs
 
 
-def get_spiral_xzs(apothem):
-    # spiral from center
-    # the run lengths are 1,1,2,2,3,3,4,4,... in the four directions
-    directions = [(1,0),(0,1),(-1,0),(0,-1)]
-    start_xz = (0,0)
-    xz = start_xz
-    yield start_xz
-
-    step_i = 0
-    while True:
-        dx = abs(xz[0] - start_xz[0])
-        if dx >= apothem:
-            break
-
-        direction_i = step_i % 4
-        direction = directions[direction_i]
-        run_length = step_i // 2 + 1  # 0>1, 1>1, 2>2, 3>2, 4>3, ...
-        for run_i in range(run_length):
-            x,z = xz
-            dir_x, dir_z = direction
-            x += dir_x
-            z += dir_z
-            xz = (x,z)
-            yield xz
-        step_i += 1
-
 
 def generate_maze(mc, player_pos, block_type):
     px, py, pz = player_pos
