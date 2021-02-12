48f5bdf8dd25fde0cb3c40dc6f3d55ad02ad2414
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2021-02-12 07:31:42 +0000

    semantic map generation

--- SemanticMapGenerator.py
+++ SemanticMapGenerator.py
@@ -4,6 +4,7 @@ import itertools
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 
 def get_concept_names():
@@ -56,15 +57,16 @@ def get_n_wordforms(n):
 
 
 def add_initial_forms(conceptual_space):
-    cs = conceptual_space
+    cs = deepcopy(conceptual_space)
     n_concepts = len(cs.nodes)
     wfs = get_n_wordforms(n_concepts)
     for wf, node in zip(wfs, cs.nodes):
-        g.nodes[node]["wordform"] = wf
+        cs.nodes[node]["wordform"] = wf
     return cs
 
 
 def make_semantic_change(g):
+    g = deepcopy(g)
     # select random node, either extend its meaning to a neighbor, or replace it with a novel word
     chosen_node = random.choice(list(g.nodes))  # random.choice(g.nodes) does weird thing where choice gets an int, and tries to access that int index on the iterable, but then g.nodes interprets that int index as a node name and throws KeyError
     extend = random.random() < 0.9
@@ -96,6 +98,7 @@ def make_n_semantic_changes(g, n):
 
 
 def print_form_meaning_correspondence(g):
+    print("\n-- form-meaning correspondence")
     d = {}
     for n in g.nodes:
         wf = g.nodes[n]["wordform"]
@@ -108,6 +111,13 @@ def print_form_meaning_correspondence(g):
         print("wordform '{}' codes {} concepts: {}".format(form, len(concepts), concepts_str))
 
 
+def evolve_language_on_conceptual_space(conceptual_space):
+    g0 = conceptual_space
+    g1 = add_initial_forms(g0)
+    g1 = make_n_semantic_changes(g1, 100)
+    return g1
+
+
 def draw_weighted_graph(g):
     # https://stackoverflow.com/questions/22967086/colouring-edges-by-weight-in-networkx
     edges,weights = zip(*nx.get_edge_attributes(g,'weight').items())
@@ -116,10 +126,27 @@ def draw_weighted_graph(g):
     plt.show()
 
 
+def draw_semantic_map(language_specific_graphs):
+    # for mid-edge text labels: https://stackoverflow.com/questions/47094949/labeling-edges-in-networkx
+    pos = nx.spring_layout(g)
+    nx.draw(G,pos,edge_color='black',width=1,linewidths=1,\
+    node_size=500,node_color='pink',alpha=0.9,\
+    labels={node:node for node in G.nodes()})
+    nx.draw_networkx_edge_labels(G,pos,edge_labels={('A','B'):'AB',\
+    ('B','C'):'BC',('B','D'):'BD'},font_color='red')
+    plt.show()
+
 
 if __name__ == "__main__":
-    g = create_conceptual_space(50)
+    g0 = create_conceptual_space(50)
     # draw_weighted_graph(g)
-    g = add_initial_forms(g)
-    g = make_n_semantic_changes(g, 2000)
-    print_form_meaning_correspondence(g)
+
+    g1 = evolve_language_on_conceptual_space(g0)
+    g2 = evolve_language_on_conceptual_space(g0)
+    g3 = evolve_language_on_conceptual_space(g0)
+    print_form_meaning_correspondence(g1)
+    print_form_meaning_correspondence(g2)
+    print_form_meaning_correspondence(g3)
+
+    draw_semantic_map([g1, g2, g3])
+
