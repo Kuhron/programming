b5f3ce4c17b2e3c85e779f4fcc018db5cc07907e
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2021-05-02 05:46:06 +0000

    got nice number pronunciations working using OT of all things

--- NumberPronunciation.py
+++ NumberPronunciation.py
@@ -1,22 +1,28 @@
 import random
 import itertools
+import functools
 
 
 class Segment:
     def matches_any_feature_dict(self, options):
+        # matches ANY dict, so if see a single match, return True immediately
         for option in options:
-            if not self.matches_feature_dict(option):
-                return False
-        return True
+            if self.matches_feature_dict(option):
+                return True
+        # only if NO dict was matched, return False
+        return False
 
     def matches_feature_dict(self, d):
         for k, v in d.items():
             try:
                 self_attr = getattr(self, k)
             except AttributeError:
+                # print(f"{self} has no attribute {k}")
                 return False
             if self_attr != v:
+                # print(f"{self}.{k} is {self_attr}, not {v}")
                 return False
+        # print(f"{self} matches {d}")
         return True
 
     def __repr__(self):
@@ -45,10 +51,17 @@ class BeginningMarkednessConstraint:
     def get_violations(self, word):
         n = len(self.sequence)
         substr = word[:n]
+        if len(substr) < n:
+            # substr was too short, constraint cannot apply
+            return 0
         if word_matches_sequence(substr, self.sequence):
+            # print(f"beginning substr {substr} violates markedness of {self.sequence}")
             return 1
         else:
             return 0
+
+    def __repr__(self):
+        return f"<Constraint *#{self.sequence}>"
 
 
 class EndMarkednessConstraint:
@@ -58,10 +71,37 @@ class EndMarkednessConstraint:
     def get_violations(self, word):
         n = len(self.sequence)
         substr = word[-n:]
+        if len(substr) < n:
+            # substr was too short, constraint cannot apply
+            return 0
         if word_matches_sequence(substr, self.sequence):
+            # print(f"end substr {substr} violates markedness of {self.sequence}")
             return 1
         else:
             return 0
+
+    def __repr__(self):
+        return f"<Constraint *{self.sequence}#>"
+
+
+class WholeWordMarkednessConstraint:
+    def __init__(self, sequence):
+        self.sequence = sequence
+
+    def get_violations(self, word):
+        n = len(self.sequence)
+        if len(word) != n:
+            return 0
+        substr = word
+        if word_matches_sequence(substr, self.sequence):
+            # print(f"whole word {substr} violates markedness of {self.sequence}")
+            return 1
+        else:
+            return 0
+
+    def __repr__(self):
+        return f"<Constraint *#{self.sequence}#>"
+
 
 
 class AnywhereMarkednessConstraint:
@@ -73,14 +113,23 @@ class AnywhereMarkednessConstraint:
         count = 0
         for i in range(len(word) - n + 1):
             substr = word[i:i+n]
+            if len(substr) < n:
+                # substr was too short, constraint cannot apply
+                continue
             if word_matches_sequence(substr, self.sequence):
+                # print(f"internal substr {substr} of word {word} violates markedness of {self.sequence} with badness {count}")
                 count += 1
         return count
 
+    def __repr__(self):
+        return f"<Constraint *{self.sequence}>"
+
 
 def word_matches_sequence(word, seq):
-    assert len(word) == len(seq), "can't align with different lengths"
+    assert len(word) == len(seq), f"can't align with different lengths: word {word} vs seq {seq}"
+    # word must match ALL in seq, but each segment can match ANY of the corresponding options
     for w_seg, seq_seg_options in zip(word, seq):
+        # print(f"checking if word seg {w_seg} matches options {seq_seg_options}")
         if w_seg.matches_any_feature_dict(seq_seg_options):
             # it matches so far, continue
             continue
@@ -112,6 +161,7 @@ def get_optimal_candidate(candidates, CONSTRAINTS):
     # strict ranking
     candidates_left = [x for x in candidates]
     for constraint in CONSTRAINTS:
+        # print(f"evaluating candidates w.r.t. {constraint}")
         # print(f"candidates left: {candidates_left}")
         violations_by_candidate = {}
         for c in candidates_left:
@@ -120,10 +170,15 @@ def get_optimal_candidate(candidates, CONSTRAINTS):
             violations_by_candidate[c] = violations
         min_violations = min(violations_by_candidate.values())
         candidates_left = [c for c,viols in violations_by_candidate.items() if viols == min_violations]
+        candidates_removed = [c for c,viols in violations_by_candidate.items() if viols != min_violations]
         if len(candidates_left) == 1:
+            # print(f"\nreturning optimal candidate: {candidates_left[0]}")
+            # input("check")
             return candidates_left[0]
         elif len(candidates_left) == 0:
             raise RuntimeError("all candidates were rejected, problem in OT code")
+        # print(f"\ndone with constraint {constraint}\nremoved: {candidates_removed}\nleft: {candidates_left}\n")
+        # input("check")
     # if got here, we are out of constraints but must have more than one candidate left
     raise Exception(f"more than one candidate passed: {candidates_left}")
 
@@ -139,6 +194,24 @@ def get_segment_cv(seg):
 
 def get_word_cv(w):
     return "".join(get_segment_cv(seg) for seg in w)
+
+
+def conjugate_number(n, CONSONANTS, VOWELS, CONSTRAINTS):
+    print(f"root number chosen: {n}")
+    prefixes = [""] + [str(x) for x in random.sample(list(range(100)), 3)]
+    suffixes = [""] + [str(x) for x in random.sample(list(range(100)), 3)]
+
+    for prefix in prefixes:
+        for suffix in suffixes:
+            this_n = int(prefix + str(n) + suffix)
+            this_n_display = f"{prefix}-{n}-{suffix}"
+            print_pronunciation(this_n, this_n_display, CONSONANTS, VOWELS, CONSTRAINTS)
+
+
+def print_pronunciation(n_int, n_display_str, CONSONANTS, VOWELS, CONSTRAINTS):
+    pronunciation = get_pronunciation(n_int, CONSONANTS, VOWELS, CONSTRAINTS)
+    s = " ".join(x.symbol for x in pronunciation)  # join with space because some IPA symbols are wider than monospace
+    print(f"pronunciation of {n_display_str}: {s}")
 
 
 if __name__ == "__main__":
@@ -182,84 +255,216 @@ if __name__ == "__main__":
     ]
     VOWELS = [[Vowel(x)] for x in "oiyeauɤɯøæ"]
 
-    c_features = {"cv": "C"}
-    v_features = {"cv": "V"}
-    stop_features = {"manner": "stop"}
-    nasal_features = {"manner": "nasal"}
-    voiceless_features = {"voicing": "voiceless"}
-    voiced_features = {"voicing": "voiced"}
-    v_y_features = {"symbol": "y"}
-    v_oe_features = {"symbol": "ø"}
-    v_ae_features = {"symbol": "æ"}
-    v_eu_features = {"symbol": "ɯ"}
-    v_eo_features = {"symbol": "ɤ"}
-    c_m_features = {"symbol": "m"}
-    c_ng_features = {"symbol": "ŋ"}
-    dental_fricative_features = {"place": "dental", "manner": "fricative"}
-    c_w_features = {"symbol": "w"}
-    c_h_features = {"symbol": "h"}
-    voiced_fricative_features = {"manner": "fricative", "voicing": "voiced"}
-    c_j_features = {"symbol": "j"}
-    v_i_features = {"symbol": "i"}
-    v_e_features = {"symbol": "e"}
-    v_a_features = {"symbol": "a"}
-    v_o_features = {"symbol": "o"}
-    v_u_features = {"symbol": "u"}
+    affricate_features = {"manner": "affricate"}
+    approximant_features = {"manner": "approximant"}
+    c_b_features = {"symbol": "b"}
     c_c_cedilla_features = {"symbol": "ç"}
+    c_curly_j_features = {"symbol": "ʝ"}
+    c_d_features = {"symbol": "d"}
+    c_dzh_features = {"symbol": "ʤ"}
+    c_eth_features = {"symbol": "ð"}
+    c_f_features = {"symbol": "f"}
+    c_features = {"cv": "C"}
+    c_g_features = {"symbol": "g"}
+    c_h_features = {"symbol": "h"}
+    c_j_features = {"symbol": "j"}
+    c_k_features = {"symbol": "k"}
+    c_l_features = {"symbol": "l"}
+    c_m_features = {"symbol": "m"}
+    c_n_features = {"symbol": "n"}
+    c_ng_features = {"symbol": "ŋ"}
+    c_p_features = {"symbol": "p"}
+    c_r_features = {"symbol": "r"}
+    c_s_features = {"symbol": "s"}
+    c_sh_features = {"symbol": "ʃ"}
+    c_t_features = {"symbol": "t"}
+    c_theta_features = {"symbol": "θ"}
+    c_tsh_features = {"symbol": "ʧ"}
+    c_v_features = {"symbol": "v"}
+    c_w_features = {"symbol": "w"}
+    c_z_features = {"symbol": "z"}
+    c_zh_features = {"symbol": "ʒ"}
+    dental_fricative_features = {"place": "dental", "manner": "fricative"}
+    palatal_fricative_features = {"place": "palatal", "manner": "fricative"}
+    postalveolar_fricative_features = {"place": "postalveolar", "manner": "fricative"}
+    lateral_approximant_features = {"manner": "lateral_approximant"}
+    nasal_features = {"manner": "nasal"}
+    stop_features = {"manner": "stop"}
+    v_a_features = {"symbol": "a"}
+    v_ae_features = {"symbol": "æ"}
+    v_e_features = {"symbol": "e"}
+    v_eo_features = {"symbol": "ɤ"}
+    v_eu_features = {"symbol": "ɯ"}
+    v_features = {"cv": "V"}
+    v_i_features = {"symbol": "i"}
+    v_o_features = {"symbol": "o"}
+    v_oe_features = {"symbol": "ø"}
+    v_u_features = {"symbol": "u"}
+    v_y_features = {"symbol": "y"}
+    voiced_affricate_features = {"manner": "affricate", "voicing": "voiced"}
+    voiced_features = {"voicing": "voiced"}
+    voiced_fricative_features = {"manner": "fricative", "voicing": "voiced"}
+    voiced_stop_features = {"manner": "stop", "voicing": "voiced"}
+    voiceless_affricate_features = {"manner": "affricate", "voicing": "voiceless"}
+    voiceless_features = {"voicing": "voiceless"}
+    voiceless_stop_features = {"manner": "stop", "voicing": "voiceless"}
 
-    c_seg = [c_features]  # if it matches anything in the list, it will match the segment selector
-    v_seg = [v_features]
-    stop_seg = [stop_features]
-    nasal_seg = [nasal_features]
-    voiced_seg = [voiced_features]
-    voiceless_seg = [voiceless_features]
-    non_canonical_vowel_seg = [v_y_features, v_oe_features, v_ae_features, v_eu_features, v_eo_features]
-    optional_nasal_seg = [c_m_features, c_ng_features]
-    dental_fricative_seg = [dental_fricative_features]
-    W_seg = [c_w_features]
-    H_seg = [c_h_features]
-    voiced_fricative_seg = [voiced_fricative_features]
+    B_seg = [c_b_features]
     CCEDILLA_seg = [c_c_cedilla_features]
-    J_seg = [c_j_features]
+    CURLYJ_seg = [c_curly_j_features]
+    DZH_seg = [c_dzh_features]
+    D_seg = [c_d_features]
+    ETH_seg = [c_eth_features]
+    F_seg = [c_f_features]
+    G_seg = [c_g_features]
+    H_seg = [c_h_features]
     I_seg = [v_i_features]
+    J_seg = [c_j_features]
+    K_seg = [c_k_features]
+    L_seg = [c_l_features]
+    M_seg = [c_m_features]
+    NG_seg = [c_ng_features]
+    N_seg = [c_n_features]
+    P_seg = [c_p_features]
+    R_seg = [c_r_features]
+    SH_seg = [c_sh_features]
+    S_seg = [c_s_features]
+    THETA_seg = [c_theta_features]
+    TSH_seg = [c_tsh_features]
+    T_seg = [c_t_features]
+    V_seg = [c_v_features]
+    W_seg = [c_w_features]
+    ZH_seg = [c_zh_features]
+    Z_seg = [c_z_features]
+    affricate_seg = [affricate_features]
+    c_seg = [c_features]  # if it matches anything in the list, it will match the segment selector
+    dental_fricative_seg = [dental_fricative_features]
+    nasal_seg = [nasal_features]
+    non_canonical_vowel_seg = [v_y_features, v_oe_features, v_ae_features, v_eu_features, v_eo_features]
+    non_canonical_consonant_seg = [affricate_features, dental_fricative_features, postalveolar_fricative_features, palatal_fricative_features]
+    optional_nasal_seg = [c_m_features, c_ng_features]
+    sonorant_seg = [nasal_features, approximant_features, lateral_approximant_features]
+    stop_seg = [stop_features]
+    v_seg = [v_features]
+    voiced_fricative_seg = [voiced_fricative_features]
+    voiced_seg = [voiced_features]
+    voiced_stop_or_affricate_seg = [voiced_stop_features, voiced_affricate_features]
+    voiceless_seg = [voiceless_features]
+    voiceless_stop_or_affricate_seg = [voiceless_stop_features, voiceless_affricate_features]
+    voiceless_stop_seg = [voiceless_stop_features]
 
-    c_seq = [c_seg]  # a sequence of one segment
-    v_seq = [v_seg]
-    cc_seq = [c_seg, c_seg]  # this is a list of segments in order
-    vv_seq = [v_seg, v_seg]
-    v_voiceless_v_seq = [v_seg, voiceless_seg, v_seg]
-    stop_seq = [stop_seg]
-    non_canonical_vowel_seq = [non_canonical_vowel_seg]
-    optional_nasal_seq = [optional_nasal_seg]
-    v_stop_v_seq = [v_seg, stop_seg, v_seg]
-    dental_fricative_seq = [dental_fricative_seg]
-    W_seq = [W_seg]
-    H_seq = [H_seg]
-    voiced_fricative_seq = [voiced_fricative_seg]
+    B_seq = [B_seg]
     CCEDILLA_seq = [CCEDILLA_seg]
+    CURLYJ_seq = [CURLYJ_seg]
+    DZH_seq = [DZH_seg]
+    D_seq = [D_seg]
+    ETH_seq = [ETH_seg]
+    F_seq = [F_seg]
+    G_seq = [G_seg]
+    H_seq = [H_seg]
+    I_J_seq = [I_seg, J_seg]
     J_I_seq = [J_seg, I_seg]
+    K_seq = [K_seg]
+    L_seq = [L_seg]
+    M_seq = [M_seg]
+    NG_seq = [NG_seg]
+    N_seq = [N_seg]
+    P_seq = [P_seg]
+    R_seq = [R_seg]
+    SH_seq = [SH_seg]
+    S_seq = [S_seg]
+    THETA_seq = [THETA_seg]
+    TSH_seq = [TSH_seg]
+    T_seq = [T_seg]
+    V_seq = [V_seg]
+    W_seq = [W_seg]
+    ZH_seq = [ZH_seg]
+    Z_seq = [Z_seg]
+    affricate_seq = [affricate_seg]
+    c_seq = [c_seg]  # a sequence of one segment
+    cc_seq = [c_seg, c_seg]  # this is a list of segments in order
+    dental_fricative_seq = [dental_fricative_seg]
+    non_canonical_vowel_seq = [non_canonical_vowel_seg]
+    non_canonical_consonant_seq = [non_canonical_consonant_seg]
+    optional_nasal_seq = [optional_nasal_seg]
+    stop_seq = [stop_seg]
+    v_nasal_v_seq = [v_seg, nasal_seg, v_seg]
+    v_seq = [v_seg]
+    v_stop_v_seq = [v_seg, stop_seg, v_seg]
+    v_voiceless_v_seq = [v_seg, voiceless_seg, v_seg]
+    voiced_fricative_seq = [voiced_fricative_seg]
+    voiced_seq = [voiced_seg]
+    voiced_stop_or_affricate_seq = [voiced_stop_or_affricate_seg]
+    voiced_voiceless_seq = [voiced_seg, voiceless_seg]
+    voiceless_seq = [voiceless_seg]
+    voiceless_stop_or_affricate_seq = [voiceless_stop_or_affricate_seg]
+    voiceless_stop_seq = [voiceless_stop_seg]
+    voiceless_voiced_seq = [voiceless_seg, voiced_seg]
+    vv_seq = [v_seg, v_seg]
+
 
     CONSTRAINTS = [
-        AnywhereMarkednessConstraint(non_canonical_vowel_seq),
-        BeginningMarkednessConstraint(optional_nasal_seq),
-        BeginningMarkednessConstraint(voiced_fricative_seq),
+        WholeWordMarkednessConstraint(c_seq),
+        # BeginningMarkednessConstraint(cc_seq),
+        # BeginningMarkednessConstraint(vv_seq),
         AnywhereMarkednessConstraint(cc_seq),
         AnywhereMarkednessConstraint(vv_seq),
+        AnywhereMarkednessConstraint(voiceless_voiced_seq),
+        AnywhereMarkednessConstraint(voiced_voiceless_seq),
+
+        AnywhereMarkednessConstraint(non_canonical_vowel_seq),
+        BeginningMarkednessConstraint(optional_nasal_seq),
+        BeginningMarkednessConstraint(voiceless_stop_or_affricate_seq),
+        BeginningMarkednessConstraint(voiced_fricative_seq),
+        BeginningMarkednessConstraint(R_seq),
         AnywhereMarkednessConstraint(v_voiceless_v_seq),
+        AnywhereMarkednessConstraint(v_nasal_v_seq),
         AnywhereMarkednessConstraint(v_stop_v_seq),
-        AnywhereMarkednessConstraint(dental_fricative_seq),
+        EndMarkednessConstraint(dental_fricative_seq),
         AnywhereMarkednessConstraint(W_seq),
         AnywhereMarkednessConstraint(H_seq),
         EndMarkednessConstraint(stop_seq),
 
         AnywhereMarkednessConstraint(J_I_seq),
+        EndMarkednessConstraint(I_J_seq),
+        AnywhereMarkednessConstraint(CURLYJ_seq),
         AnywhereMarkednessConstraint(CCEDILLA_seq),
+        AnywhereMarkednessConstraint(non_canonical_consonant_seq),
 
+        EndMarkednessConstraint(voiced_seq),
         BeginningMarkednessConstraint(v_seq),
+        # TODO add "goodness" constraints that encourage double consonants e.g. ll, nn, tt, kk
+
+
+        # low-ranked stuff that I'm not actually using for my ordering, but which can be used in random orderings
+        AnywhereMarkednessConstraint(B_seq),
+        AnywhereMarkednessConstraint(D_seq),
+        AnywhereMarkednessConstraint(DZH_seq),
+        AnywhereMarkednessConstraint(ETH_seq),
+        AnywhereMarkednessConstraint(F_seq),
+        AnywhereMarkednessConstraint(G_seq),
+        AnywhereMarkednessConstraint(K_seq),
+        AnywhereMarkednessConstraint(L_seq),
+        AnywhereMarkednessConstraint(M_seq),
+        AnywhereMarkednessConstraint(N_seq),
+        AnywhereMarkednessConstraint(NG_seq),
+        AnywhereMarkednessConstraint(P_seq),
+        AnywhereMarkednessConstraint(S_seq),
+        AnywhereMarkednessConstraint(SH_seq),
+        AnywhereMarkednessConstraint(T_seq),
+        AnywhereMarkednessConstraint(THETA_seq),
+        AnywhereMarkednessConstraint(TSH_seq),
+        AnywhereMarkednessConstraint(V_seq),
+        AnywhereMarkednessConstraint(Z_seq),
+        AnywhereMarkednessConstraint(ZH_seq),
+
     ]
 
+    # random.shuffle(CONSTRAINTS)
     n = random.randint(0, 9999)
-    print(f"number chosen: {n}")
-    pronunciation = get_pronunciation(n, CONSONANTS, VOWELS, CONSTRAINTS)
-    s = "".join(x.symbol for x in pronunciation)
-    print(f"pronunciation: {s}")
+    conjugate_number(n, CONSONANTS, VOWELS, CONSTRAINTS)
+
+    for n in range(1000):
+        print_pronunciation(n, str(n), CONSONANTS, VOWELS, CONSTRAINTS)
+
+
