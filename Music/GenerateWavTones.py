f76ccf26623f5a4947e819fd9519655d6196257a
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2021-05-20 02:47:04 +0000

    creating sin wave tones for linux-clicky modification

---
+++ Music/GenerateWavTones.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.insert(0,'..')
+
+import Music.WavUtil as wav
+import Music.MidiUtil as midi_util
+
+
+for note_number in range(0, 89):
+    fp = f"/home/wesley/WavTones/{note_number}.wav"
+    hz = midi_util.note_number_to_hertz(note_number)
+    print(f"note number {note_number} is {hz} Hz")
+    signal = wav.get_signal_from_freq(hz, seconds=0.1, truncate=False)
+    print(signal.shape)
+    wav.write_signal_to_wav(signal, fp)
