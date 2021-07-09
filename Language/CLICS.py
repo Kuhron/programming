60c5f9a91b926f8be33e2460aee9e84f5204d273
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2021-07-09 21:04:47 +0000

    downloaded CLICS database (colexifications)

---
+++ Language/CLICS.py
@@ -0,0 +1,4 @@
+# messing around with colexifications using CLICS database
+# citation: Rzymski, Tresoldi et al. 2019. The Database of Cross-Linguistic Colexifications, reproducible analysis of cross- linguistic polysemies. DOI: doi.org/10.17613/5awv-6w15
+
+
