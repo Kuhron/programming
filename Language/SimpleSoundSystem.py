fd3acffd70547d9d6cfeb970ef938ae34c189e0f
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2021-11-01 22:34:54 +0000

    standardized word vector for NN

--- Language/SimpleSoundSystem.py
+++ Language/SimpleSoundSystem.py
@@ -117,8 +117,60 @@ class SoundVectorSeries:
         return f"<{self.bit_string} = {self.string}>"
 
 
+class FiveSyllableSoundVector:
+    # for machine learning, pad all words, force them to be max of five syllables
+    # for each syllable, there is an extra bit that says whether the syllable is actually used
+    # need to adjust any random inputs such that the first is-used bit is always 1,
+    # - any is-used bits after the first 0 are also 0
+    # - all other bits in an unused syllable are 0
+    # this will force a given surface form to only have a unique bit form, which will be good for the machine learning so it's not finding stuff in the unused bits that shouldn't matter
+
+    def __init__(self, bits):
+        assert type(bits) is np.ndarray, type(bits)
+        assert bits.shape == (5 * 7,), bits.shape
+        bits = FiveSyllableSoundVector.cleanup_bits(bits)  # validate and change any aberrant bits
+        self.bits = bits
+        self.bit_string = "".join(self.bits.astype(str))
+        self.syllable_sound_vectors = [SoundVector(self.bits[i*7 + 1 : (i+1)*7]) if self.bits[i*7] else None for i in range(5)]
+        self.string = "".join(syll.string if syll is not None else "X" for syll in self.syllable_sound_vectors)
+
+    @staticmethod
+    def random():
+        bits = np.random.choice([0, 1], (35,))
+        # do the validation in the initialization
+        return FiveSyllableSoundVector(bits)
+
+    @staticmethod
+    def cleanup_bits(bits):
+        new_bits = [b for b in bits]
+        new_bits[0] = 1  # force the first is-on bit to be true so word is non-empty
+
+        # find the first is-not-used and zero out everything from then on
+        n_syllables = None
+        for i in range(1, 5):
+            is_on = bits[i*7]
+            if not is_on:
+                n_syllables = i  # this will be the number of syllables seen before, e.g. i=2 is the third iteration so we've already seen two before this
+                break
+        if n_syllables is None:
+            # never got an is-not-used, so all syllables are on
+            n_syllables = 5
+        assert 1 <= n_syllables <= 5
+
+        # now zero out everything starting with the first is-not-used
+        is_not_used_index = 7 * n_syllables
+        for i in range(is_not_used_index, len(bits)):
+            new_bits[i] = 0
+
+        return np.array(new_bits)
+
+
 if __name__ == "__main__":
-    singulars = [SoundVectorSeries.random() for i in range(100)]
-    plurals = [SoundVectorSeries.random() for i in range(100)]
-    for s, p in zip(singulars, plurals):
-        print(f"the plural of {s.string} is {p.string}")
+    # singulars = [SoundVectorSeries.random() for i in range(100)]
+    # plurals = [SoundVectorSeries.random() for i in range(100)]
+    # for s, p in zip(singulars, plurals):
+    #     print(f"the plural of {s.string} is {p.string}")
+
+    for i in range(100):
+        v = FiveSyllableSoundVector.random()
+        print(f"{v.bit_string} = {v.string}")
