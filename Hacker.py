9c112d0cff8ed88140ecfa9f3a38ea3437d3f085
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2022-04-20 20:53:12 +0000

    leet hax

---
+++ Hacker.py
@@ -0,0 +1,21 @@
+import random
+import os
+from pathlib import Path
+
+print("loading...")
+
+directory = "/home/wesley/programming/"
+
+py_files = list(Path(directory).rglob('*.py'))
+
+py_lines = []
+for fp in py_files:
+    print(f"loading {fp}")
+    with open(fp) as f:
+        lines = f.readlines()
+    for line in lines:
+        py_lines.append(line.replace("\n", ""))
+
+while True:
+    line = random.choice(py_lines)
+    print(line)
