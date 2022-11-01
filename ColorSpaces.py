238ada09e09c33ddfdeb206cacbcb15ad9abff49
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2022-11-01 19:09:31 +0000

    color space idea

---
+++ ColorSpaces.py
@@ -0,0 +1,12 @@
+# idea to play with perceptual color spaces
+# etic colors are a set of wavelengths, each with an associated intensity
+# equivalently, intensity as a function of wavelengths
+# so etic colors live in R^infinity
+# emic colors are based on the receptors you have
+# each receptor has a responsivity function of wavelength
+# emic colors are the activations of the receptors
+# so emic colors live in R^n_receptors
+# so sometimes different etic colors will create the same emic color
+# it would be fun to play with these equivalence classes
+# e.g. what if I have several receptors with bimodal activations (two normal distributions added together, say), what do the equivalence classes look like?
+
