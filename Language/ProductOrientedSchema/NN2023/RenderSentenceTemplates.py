a3b107c8aec73cfd1b0ffe4a382b375ee6b3a66b
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2023-01-01 22:22:07 +0000

    working on idea for product-oriented schema program

---
+++ Language/ProductOrientedSchema/NN2023/RenderSentenceTemplates.py
@@ -0,0 +1,61 @@
+import re
+
+from TemplateWordGroups import template_word_groups
+
+template_fp = "sentence_templates.txt"
+translation_fp = "word_translations.txt"
+
+
+def get_sentence_template_strings():
+    with open(template_fp) as f:
+        lines = f.readlines()
+    l2 = [l.strip() for l in lines]
+    return [l for l in l2 if len(l) > 0]
+
+
+def get_translation_dict():
+    with open(translation_fp) as f:
+        lines = f.readlines()
+    pairs = [l.replace("\n","").split(" = ") for l in lines]
+    eng_to_lang = dict(pairs)
+    return eng_to_lang
+
+
+def render_template_all_possibilities_eng(template_str):
+    pattern = "\{[^\{\}]+\}"
+    first_match = re.search(pattern, template_str)
+    if first_match is None:
+        return [template_str]
+    else:
+        first_match = first_match.group()  # get the text
+    group_name = first_match.replace("{","").replace("}","")
+    replacements = template_word_groups[group_name]
+    renditions = [template_str.replace(first_match, replacement) for replacement in replacements]
+    final_renditions = []
+    for s in renditions:
+        final_renditions += render_template_all_possibilities_eng(s)
+    return final_renditions
+
+
+def render_template_all_possibilities_lang(template_str, d):
+    eng = render_template_all_possibilities_eng(template_str)
+    lang = [translate_gloss(s, d) for s in eng]
+    return lang
+
+
+def translate_gloss(s, d):
+    words = s.split()
+    w2 = [d[w] for w in words]
+    s2 = " ".join(w2)
+    return s2
+
+
+if __name__ == "__main__":
+    templates = get_sentence_template_strings()
+    translations = get_translation_dict()
+    for template in templates:
+        print(f"\ntemplate: {template}")
+        renditions = render_template_all_possibilities_lang(template, translations)
+        for s in renditions:
+            print(s)
+    
---
+++ Language/ProductOrientedSchema/NN2023/TemplateWordGroups.py
@@ -0,0 +1,8 @@
+template_word_groups = {
+    "subject_pronoun": ["1sg", "2sg", "3sg", "1pl", "2pl", "3pl"],
+    "food_item": ["food", "water", "fruit", "meat"],
+    "thing": ["{inanimate}", "{animate}", "{subject_pronoun}"],
+    "animate": ["dog", "cat", "person"],
+    "inanimate": ["tree", "rock", "{food_item}"],
+    "tense": ["prs", "pst"],
+}
