223fa9fa4bd17d7457b2720071b7d1708934efde
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2023-01-03 19:10:39 +0000

    switching to bigger laptop for fitting the actual neural nets

--- Language/ProductOrientedSchema/NN2023/FitNNs.py
+++ Language/ProductOrientedSchema/NN2023/FitNNs.py
@@ -1,15 +1,20 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+import keras
 
 import FitWord2Vec as w2v
 import ArticulatoryFeatures as af
 import RenderSentenceTemplates as rst
 
-# TODO download keras
-
 
 def fit_nn(inp, outp):
-    raise NotImplementedError
+    n_words, n_inp_features = inp.shape
+    n_words_outp, n_outp_features = outp.shape
+    assert n_words == n_words_outp, "mismatch in number of words"
+    input_layer = keras.Input(n_inp_features)
+    model = keras.Model(input_layer, outp)
+    
 
 
 
@@ -26,10 +31,12 @@ if __name__ == "__main__":
         eng_words.append(eng_word)
     lang_words = rst.translate_words(eng_words, translations)
 
+    # want articulation in the language, but semantics from the unambiguous glosses
     articulation_arr = af.convert_words_to_articulatory_nn_input(lang_words)
-    print(articulation_arr.shape)
+    # print(articulation_arr.shape)
     semantic_arr = w2v.convert_words_to_semantic_nn_input(eng_words, eng_text_tokens)
-    print(semantic_arr.shape)
+    # print(semantic_arr.shape)
 
     nn_sound_to_meaning = fit_nn(articulation_arr, semantic_arr)
     nn_meaning_to_sound = fit_nn(semantic_arr, articulation_arr)
+    
