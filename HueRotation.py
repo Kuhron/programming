172833b35ee32fc38acbae3aa4e6ffff93d38892
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2023-03-28 01:15:28 +0000

    writing RGB to HSV conversion (and vice versa) myself to bypass slow np.vectorize calls

--- HueRotation.py
+++ HueRotation.py
@@ -5,21 +5,141 @@ import os
 import colorsys
 
 from ImageDatasetLoadPhonePhotos import get_random_image_fps
+from PerceptualHueScale import get_standard_hue_from_desired_01, get_desired_hue_from_standard_01
+
+
+def rgb_to_hsv(r, g, b):
+    # since vectorizing colorsys.rgb_to_hsv is slow
+    # formulas: https://www.rapidtables.com/convert/color/rgb-to-hsv.html
+    assert r.shape == g.shape == b.shape
+    assert (0 <= r).all() and (r <= 1).all()
+    assert (0 <= g).all() and (g <= 1).all()
+    assert (0 <= b).all() and (b <= 1).all()
+
+    cmax = np.maximum(np.maximum(r, g), b)
+    cmin = np.minimum(np.minimum(r, g), b)
+    delta = cmax - cmin
+
+    h = np.zeros(r.shape)
+    h[delta == 0] = 0
+    r_mask = (cmax == r) & (delta != 0)
+    g_mask = (cmax == g) & (delta != 0)
+    b_mask = (cmax == b) & (delta != 0)
+
+    with np.errstate(divide="ignore", invalid="ignore"):
+        h[r_mask] = (60/360 * (((g-b)/delta) % 6))[r_mask]  # mask this one too so it can assign without throwing error that boolean indexing assignment needs to be 0D or 1D
+        h[g_mask] = (60/360 * ((b-r)/delta + 2))[g_mask]
+        h[b_mask] = (60/360 * ((r-g)/delta + 4))[b_mask]
+
+        s = np.zeros(r.shape)
+        s[cmax == 0] = 0
+        s[cmax != 0] = (delta/cmax)[cmax != 0]
+
+    v = cmax
+
+    return h, s, v
+
+
+def hsv_to_rgb(h, s, v):
+    # since vectorizing colorsys.hsv_to_rgb is slow
+    # formulas: https://www.rapidtables.com/convert/color/hsv-to-rgb.html
+    assert h.shape == s.shape == v.shape
+    assert (0 <= h).all() and (h < 1).all()  # h strictly less than 360 deg
+    assert (0 <= s).all() and (s <= 1).all()
+    assert (0 <= v).all() and (v <= 1).all()
+    c = v * s
+    x = c * (1 - abs((h/(60/360)) % 2 - 1))
+    m = v - c
+
+    r = np.zeros(h.shape)
+    g = np.zeros(h.shape)
+    b = np.zeros(h.shape)
+
+    h_0_60    = (0       <= h) & (h < 60/360)
+    h_60_120  = (60/360  <= h) & (h < 120/360)
+    h_120_180 = (120/360 <= h) & (h < 180/360)
+    h_180_240 = (180/360 <= h) & (h < 240/360)
+    h_240_300 = (240/360 <= h) & (h < 300/360)
+    h_300_360 = (300/360 <= h) & (h < 1)
+
+    r[h_0_60]    = c[h_0_60]
+    r[h_60_120]  = x[h_60_120]
+    r[h_120_180] = 0
+    r[h_180_240] = 0
+    r[h_240_300] = x[h_240_300]
+    r[h_300_360] = c[h_300_360]
+
+    g[h_0_60]    = x[h_0_60]
+    g[h_60_120]  = c[h_60_120]
+    g[h_120_180] = c[h_120_180]
+    g[h_180_240] = x[h_180_240]
+    g[h_240_300] = 0
+    g[h_300_360] = 0
+
+    b[h_0_60]    = 0
+    b[h_60_120]  = 0
+    b[h_120_180] = x[h_120_180]
+    b[h_180_240] = c[h_180_240]
+    b[h_240_300] = c[h_240_300]
+    b[h_300_360] = x[h_300_360]
+
+    r = r + m
+    g = g + m
+    b = b + m
+
+    return r, g, b
+
+
+def test_color_space_conversion(R,G,B):
+    rh1 = np.vectorize(colorsys.rgb_to_hsv)
+    hr1 = np.vectorize(colorsys.hsv_to_rgb)
+    rh2 = rgb_to_hsv
+    hr2 = hsv_to_rgb
+    H1, S1, V1 = rh1(R,G,B)
+    H2, S2, V2 = rh2(R,G,B)
+    assert (abs(H1-H2) < 1e-9).all(), abs(H1-H2).max()
+    assert (abs(S1-S2) < 1e-9).all(), abs(S1-S2).max()
+    assert (abs(V1-V2) < 1e-9).all(), abs(V1-V2).max()
+    H,S,V = H1, S1, V1
+    R1, G1, B1 = hr1(H,S,V)
+    R2, G2, B2 = hr2(H,S,V)
+
+    # debug
+    # r2_bad_mask = abs(R-R2) >= 1e-9
+    # print("R", R[r2_bad_mask])
+    # print("G", G[r2_bad_mask])
+    # print("B", B[r2_bad_mask])
+    # print("H", H[r2_bad_mask])
+    # print("S", S[r2_bad_mask])
+    # print("V", V[r2_bad_mask])
+    # print("R1", R1[r2_bad_mask])
+    # print("R2", R2[r2_bad_mask])
+
+    assert (abs(R - R1) < 1e-9).all(), abs(R-R1).max()
+    assert (abs(R - R2) < 1e-9).all(), abs(R-R2).max()
+    assert (abs(G - G1) < 1e-9).all(), abs(G-G1).max()
+    assert (abs(G - G2) < 1e-9).all(), abs(G-G2).max()
+    assert (abs(B - B1) < 1e-9).all(), abs(B-B1).max()
+    assert (abs(B - B2) < 1e-9).all(), abs(B-B2).max()
+    print("color space conversion test passed\n")
 
 
 def rotate_hue(im, rotation_deg):
-    rgb_to_hsv = np.vectorize(colorsys.rgb_to_hsv)
-    hsv_to_rgb = np.vectorize(colorsys.hsv_to_rgb)
+    # rgb_to_hsv = np.vectorize(colorsys.rgb_to_hsv)
+    # hsv_to_rgb = np.vectorize(colorsys.hsv_to_rgb)
     im = Image.open(fp)
     arr = np.array(im)
     # arr = arr[:100,:100,:]  # debug
     arr = arr / 256
     assert (0 <= arr).all() and (arr <= 1).all(), f"min: {arr.min()}, max: {arr.max()}"
     R,G,B = arr[:, :, 0], arr[:, :, 1], arr[:, :, 2]
+    test_color_space_conversion(R,G,B)  # debug
     H,S,V = rgb_to_hsv(R,G,B)
     # print("hue min:", H.min(), ", hue max:", H.max())
     rotation_01 = 1/360 * rotation_deg
-    new_H = ((H + rotation_01) % 1)
+    H_perceptual = get_desired_hue_from_standard_01(H)
+    new_H_perceptual = ((H_perceptual + rotation_01) % 1)
+    new_H = get_standard_hue_from_desired_01(new_H_perceptual)
     new_R, new_G, new_B = hsv_to_rgb(new_H, S, V)
     new_arr = np.zeros(arr.shape)
     new_arr[:, :, 0] = new_R
@@ -33,16 +153,33 @@ def rotate_hue(im, rotation_deg):
 
 
 if __name__ == "__main__":
-    n_images = 500
-    fps = get_random_image_fps(n_images)
+    photo_dir = "/home/wesley/Desktop/IPhone Media/IPhone Media Temp Storage/"
+    test_photos = [
+        # "2022-03-30/20220330_230210.jpg",
+        "2022-11-12/20221112_221842.jpg",
+        "2022-10-17/20221017_234754.jpg",
+        "2022-10-15/20221015_210842.jpg",
+        "2022-09-14/20220914_001143.jpg",
+    ]
+    fps = [os.path.join(photo_dir, fp) for fp in test_photos]
+
+    R = np.random.random((5000,))
+    G = np.random.random((5000,))
+    B = np.random.random((5000,))
+    test_color_space_conversion(R,G,B)
+
+    # n_images = 500
+    # fps = get_random_image_fps(n_images)
     output_dir = "Images/HueRotation"
     for fp in fps:
-        rotation_deg = random.randrange(360)
-        print(f"rotating hue by {rotation_deg} deg: {fp}")
-        im = Image.open(fp)
-        im = rotate_hue(im, rotation_deg)
-        fname = os.path.basename(fp)
-        new_fname = f"{rotation_deg}deg_" + fname
-        new_fp = os.path.join(output_dir, new_fname)
-        print(f"saved to {new_fp}")
-        im.save(new_fp)
+        # rotation_deg = random.randrange(360)
+        for rotation_deg in range(0, 360, 15):
+            print(f"rotating hue by {rotation_deg} deg: {fp}")
+            im = Image.open(fp)
+            im = rotate_hue(im, rotation_deg)
+            fname = os.path.basename(fp)
+            new_fname = f"{rotation_deg}deg_" + fname
+            new_fp = os.path.join(output_dir, new_fname)
+            print(f"saved to {new_fp}")
+            im.save(new_fp)
+
--- PerceptualHueScale.py
+++ PerceptualHueScale.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import CubicSpline
+from scipy.interpolate import CubicSpline, interp1d
 
 
 standard_hues = {
@@ -17,7 +17,7 @@ standard_hues = {
     "red2": 360,
 }
 
-desired_hues = {
+ryb_color_wheel_hues = {
     "red": 0,
     "orange": 60,
     "yellow": 120,
@@ -26,6 +26,19 @@ desired_hues = {
     "violet": 300,
     "red2": 360,
 }
+
+experimental_hues = {
+    "red": 0,
+    "orange": 80,
+    "yellow": 160,
+    "green": 240,
+    "blue": 280,
+    "violet": 320,
+    "red2": 360,
+}
+
+# desired_hues = ryb_color_wheel_hues  # still too much green and purple!
+desired_hues = experimental_hues
 
 # want a function mapping between these, one to one, use spline or something, ideally it's smooth, same derivative on both ends, since this loops around
 
@@ -49,45 +62,92 @@ spl_des_to_stan = CubicSpline(xs_des_to_stan, ys_des_to_stan_adjusted, bc_type="
 
 xs = np.linspace(0, 360, 360*60)
 
-plt.plot(xs, spl_stan_to_des(xs), c="b")
-plt.plot(xs, -1*spl_des_to_stan(xs), c="r")
-plt.show()
+spl_ys_stan_to_des_adjusted = spl_stan_to_des(xs)
+spl_ys_des_to_stan_adjusted = spl_des_to_stan(xs)
 
-spl_ys_stan_to_des = spl_stan_to_des(xs)
-spl_ys_des_to_stan = spl_des_to_stan(xs)
+spl_ys_stan_to_des = xs + spl_ys_stan_to_des_adjusted
+spl_ys_des_to_stan = xs + spl_ys_des_to_stan_adjusted
 
-# avg_ys_stan_to_des = ?
-# avg_ys_des_to_stan = ?
+# now we line up the two curves along lines perpendicular to y=x, so we can average the curves "toward" the y=x line (you can't just average them in their respective coordinate worlds because adding xs creates shear on the curve's shape)
+# distance from y=x line of point (a,b) = (b-a)/sqrt(2)
+# the x=y value at which that perpendicular descends to the y=x line from the point (a,b) is (a+b)/2
+# then, given a new height d above the x=y line at effective x of x', (a,b) = (x' - d/sqrt(2), x' + d/sqrt(2))
+effective_xs_stan_to_des = 1/2 * (xs + spl_ys_stan_to_des)
+effective_xs_des_to_stan = 1/2 * (xs + spl_ys_des_to_stan)
+heights_stan_to_des = 2**(-1/2) * (spl_ys_stan_to_des - xs)
+heights_des_to_stan = 2**(-1/2) * (spl_ys_des_to_stan - xs)
 
-# plot in both "spaces"
-plt.subplot(2,2,1)
-plt.plot(xs, spl_ys_stan_to_des, c="b")
-plt.plot(spl_ys_des_to_stan, xs, c="r")
-plt.scatter(xs_stan_to_des, ys_stan_to_des, c="k")
-plt.xlabel("standard hue")
-plt.ylabel("desired hue")
+# observe how the curve subtracting y=x differs from the heights off of the y=x line (shear)
+# plt.plot(xs, spl_ys_stan_to_des_adjusted, c="b")
+# plt.plot(xs, heights_stan_to_des, c="g")
+# plt.plot(xs, spl_ys_des_to_stan_adjusted, c="r")
+# plt.plot(xs, heights_des_to_stan, c="orange")
+# plt.show()
 
-plt.subplot(2,2,2)
-plt.plot(spl_ys_stan_to_des, xs, c="b")
-plt.plot(xs, spl_ys_des_to_stan, c="r")
-plt.scatter(xs_des_to_stan, ys_des_to_stan, c="k")
-plt.xlabel("desired hue")
-plt.ylabel("standard hue")
+# now want to match up the two curves in both "spaces", one where standard is the domain and the other where desired is the domain
+# so the effective xs won't necessarily be the same because of the shear
+# for each effective x (projecting onto y=x line), want to average the curves' heights at that point, thus getting a new height at this effective x, then transform that back into an (x,y) value using the formula for (a,b) above
 
-# in the lower row, plot the resulting function
-plt.subplot(2,2,3)
-# plt.plot(xs, avg_ys_stan_to_des, c="b")
-# plt.plot(avg_ys_des_to_stan, xs, c="r")
-plt.scatter(xs_stan_to_des, ys_stan_to_des, c="k")
-plt.xlabel("standard hue")
-plt.ylabel("desired hue")
+# observe how the effective xs differ, in some places the reds are closer together than the blues and in others they are farther apart, and they rarely line up exactly, this is because of changes in the slopes causing different amounts of shear
+# plt.scatter(effective_xs_stan_to_des, effective_xs_stan_to_des, c="b")
+# plt.scatter(effective_xs_des_to_stan, effective_xs_des_to_stan, c="r")
+# plt.show()
 
-plt.subplot(2,2,4)
-# plt.plot(avg_ys_stan_to_des, xs, c="b")
-# plt.plot(xs, avg_ys_des_to_stan, c="r")
-plt.scatter(xs_des_to_stan, ys_des_to_stan, c="k")
-plt.xlabel("desired hue")
-plt.ylabel("standard hue")
+# could just linearly interpolate at each effective x in the whole set?
+all_effective_xs = np.array(sorted(set(effective_xs_stan_to_des) | set(effective_xs_des_to_stan)))
+# interpolate it at the same xs values that are evenly spaced, but use the (effective_x, height) points as the guide to interpolate from
+height_by_effective_x_stan_to_des_func = interp1d(effective_xs_stan_to_des, heights_stan_to_des)
+height_by_effective_x_des_to_stan_func = interp1d(effective_xs_des_to_stan, heights_des_to_stan)
 
-plt.show()
+# for here, define height as up above y=x from the perspective of the x axis being standard hue
+heights_at_xs_stan_to_des = height_by_effective_x_stan_to_des_func(xs)
+heights_at_xs_des_to_stan = -1 * height_by_effective_x_des_to_stan_func(xs)
+average_heights_at_xs = 1/2 * (heights_at_xs_stan_to_des + heights_at_xs_des_to_stan)
+
+# because of choosing the convention where positive height = des > stan (above the y=x line from the perspective of the x axis being standard hue)
+new_stans = xs - average_heights_at_xs/(2**0.5)
+new_dess = xs + average_heights_at_xs/(2**0.5)
+
+get_average_height_at_x = lambda x: 1/2 * (height_by_effective_x_stan_to_des_func(x) + -1*height_by_effective_x_des_to_stan_func(x))
+get_standard_hue_from_desired = lambda x: x - get_average_height_at_x(x)/(2**0.5)
+get_desired_hue_from_standard = lambda x: x + get_average_height_at_x(x)/(2**0.5)
+get_standard_hue_from_desired_01 = lambda x: 1/360 * get_standard_hue_from_desired(360*x)
+get_desired_hue_from_standard_01 = lambda x: 1/360 * get_desired_hue_from_standard(360*x)
+
+# done! now we have the function we want
+
+if __name__ == "__main__":
+    # plot in both "spaces"
+    plt.subplot(2,2,1)
+    plt.plot(xs, spl_ys_stan_to_des, c="b")
+    plt.plot(xs, spl_ys_stan_to_des_adjusted, c="b", alpha=0.5)
+    plt.plot(spl_ys_des_to_stan, xs, c="r")
+    plt.plot(spl_ys_des_to_stan_adjusted, xs, c="r", alpha=0.5)
+    plt.scatter(xs_stan_to_des, ys_stan_to_des, c="k")
+    plt.xlabel("standard hue")
+    plt.ylabel("desired hue")
+
+    plt.subplot(2,2,2)
+    plt.plot(spl_ys_stan_to_des, xs, c="b")
+    plt.plot(spl_ys_stan_to_des_adjusted, xs, c="b", alpha=0.5)
+    plt.plot(xs, spl_ys_des_to_stan, c="r")
+    plt.plot(xs, spl_ys_des_to_stan_adjusted, c="r", alpha=0.5)
+    plt.scatter(xs_des_to_stan, ys_des_to_stan, c="k")
+    plt.xlabel("desired hue")
+    plt.ylabel("standard hue")
+
+    # in the lower row, plot the resulting function
+    plt.subplot(2,2,3)
+    plt.scatter(xs_stan_to_des, ys_stan_to_des, c="k")
+    plt.plot(new_stans, new_dess, c="orange")
+    plt.xlabel("standard hue")
+    plt.ylabel("desired hue")
+
+    plt.subplot(2,2,4)
+    plt.scatter(xs_des_to_stan, ys_des_to_stan, c="k")
+    plt.plot(new_dess, new_stans, c="orange")
+    plt.xlabel("desired hue")
+    plt.ylabel("standard hue")
+
+    plt.show()
 
