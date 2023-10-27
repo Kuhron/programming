78cd6310ed7399e3a2fa5d336724a39a68c4d594
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2023-10-27 06:27:06 +0000

    glyphs

--- Music/PlayMidiThroughPiano.py
+++ Music/PlayMidiThroughPiano.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
     data_dir = "/home/wesley/programming/Music/midi_input/YamahaP125"
 
     data = mu.load_random_data(data_dir)
-    data = mu.load_data_from_datetime_string(data_dir, "20231014-222344")
+    # data = mu.load_data_from_datetime_string(data_dir, "20231014-222344")
+    # data = mu.load_data_from_datetime_string(data_dir, "20231002-020531")
+    # good one: 20231002-020531 inverted
     drumtrack_fp = "/home/wesley/Desktop/Construction/MusicComposition/Wesley's/2023/Piano Accompaniments/COcta_20231001-200554_drumtrack.mid"
     # TODO use mido library to read the events from the drumtrack file, shift the times of the events in `data` such that its first note matches the first note of the third measure of the drumtrack: https://www.twilio.com/blog/working-with-midi-data-in-python-using-mido
 
@@ -21,9 +23,11 @@ if __name__ == "__main__":
     data = [[x, t-start_time_ms] for x,t in data if t >= start_time_ms]
 
     # mess with it
-    if random.random() < 0.5:
-        data = mu.invert_data(data, pivot=60)
+    invert = random.random() < 0.5
     offset = random.randint(-6, 6)
+
+    if invert:
+        data = mu.invert_data(data, pivot=60)
     print(f"{offset = }")
     data = mu.transpose_data(data, offset)
 
