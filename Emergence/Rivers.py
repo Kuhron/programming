2b2726e1ebd8e479e9e32feb500264d0c86f7a8f
Wesley Kuhron Jones <jazzzman627@yahoo.com>
2024-04-15 18:42:26 +0000

    idea for program I've been wanting to make but shouldn't right now

---
+++ Emergence/Rivers.py
@@ -0,0 +1,10 @@
+# idea: simulate formation of river channels by erosion
+# do it on the icosahedron lattice rather than a grid so you get more used to using that lattice's API independently of Cada mapping
+# rain falls with some distribution, can start with it being static but non-uniform (like addition of sine waves in a few directions or something like that)
+# sediment is carried by water flowing downhill
+# water can collect into lakes
+# faster flow means more erosion
+# watch how the topography evolves
+# later can allow the rain distribution to change over time, e.g. seasonality or other kinds of variation independent of the landscape, and also allow it to be affected by the landscape, e.g. rain shadow effects
+
+# TODO
